7966b88148adc5bb8c538bdca0a5cb74a18b257c
Toby Isaac <tisaac@cc.gatech.edu>
2019-01-04 13:44:10 -0500

    tao ex4: added user context

--- src/tao/examples/tutorials/ex4.c
+++ src/tao/examples/tutorials/ex4.c
@@ -1,12 +1,25 @@
 static char help[] = "Simple example to test separable objective optimizers.\n";
 
 #include <petsc.h>
+#include <petsctao.h>
+
+typedef struct _UserCtx
+{
+  Mat F;           /* matrix in least squares component $(1/2) * || F x - d ||_2^2$ */
+  Vec d;           /* RHS in least squares component $(1/2) * || F x - d ||_2^2$ */
+  PetscReal alpha; /* regularization constant applied to || x ||_p */
+  NormType p;
+}
+* UserCtx;
 
 int main (int argc, char** argv)
 {
+  UserCtx ctx;
   PetscErrorCode ierr;
 
   ierr = PetscInitialize(&argc, &argv, NULL,help);if (ierr) return ierr;
+  ierr = PetscNew(&ctx);CHKERRQ(ierr);
+  ierr = PetscFree(ctx);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
 }
