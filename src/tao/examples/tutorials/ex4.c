6415170cf38663b52a1cb36a7c7729315d06f4d2
Toby Isaac <tisaac@cc.gatech.edu>
2019-01-07 13:32:02 -0500

    tao ex4: set stencil correctly

--- src/tao/examples/tutorials/ex4.c
+++ src/tao/examples/tutorials/ex4.c
@@ -12,6 +12,7 @@ typedef struct _UserCtx
   PetscReal alpha; /* regularization constant applied to || x ||_p */
   PetscInt matops;
   NormType p;
+  PetscRandom    rctx;
 } * UserCtx;
 
 PetscErrorCode ConfigureContext(UserCtx ctx)
@@ -27,10 +28,23 @@ PetscErrorCode ConfigureContext(UserCtx ctx)
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD, NULL, "Configure separable objection example", "ex4.c");CHKERRQ(ierr);
   ierr = PetscOptionsInt("-m", "The row dimension of matrix F", "ex4.c", ctx->m, &(ctx->m), NULL);CHKERRQ(ierr);
   ierr = PetscOptionsInt("-n", "The column dimension of matrix F", "ex4.c", ctx->n, &(ctx->n), NULL);CHKERRQ(ierr);
-  ierr = PetscOptionsInt("-matrix_format","Decide format of F matrix. 0 for stencil, 1 for dense random", "ex4.c",
-		  					ctx->matops, &(ctx->matops), NULL); CHKERRQ(ierr);
+  ierr = PetscOptionsInt("-matrix_format","Decide format of F matrix. 0 for stencil, 1 for dense random", "ex4.c", ctx->matops, &(ctx->matops), NULL); CHKERRQ(ierr);
   ierr = PetscOptionsReal("-alpha", "The regularization multiplier", "ex4.c", ctx->alpha, &(ctx->alpha), NULL);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();CHKERRQ(ierr);
+  /* Creating random ctx */
+  ierr = PetscRandomCreate(PETSC_COMM_WORLD,&(ctx->rctx));CHKERRQ(ierr);
+  ierr = PetscRandomSetFromOptions(ctx->rctx);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode DestroyContext(UserCtx *ctx)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = PetscRandomDestroy(&((*ctx)->rctx)); CHKERRQ(ierr);
+  ierr = PetscFree(*ctx);CHKERRQ(ierr);
+  *ctx = NULL;
   PetscFunctionReturn(0);
 }
 
@@ -38,7 +52,6 @@ int main (int argc, char** argv)
 {
   UserCtx        ctx;
   PetscErrorCode ierr;
-  PetscRandom    rctx;
   PetscInt       Istart,Iend,i,j,Ii,J;
   PetscScalar    v;
 #if defined(PETSC_USE_LOG)
@@ -49,14 +62,10 @@ int main (int argc, char** argv)
   ierr = PetscNew(&ctx);CHKERRQ(ierr);
   ierr = ConfigureContext(ctx);CHKERRQ(ierr);
 
-  /* Creating random ctx */
-  ierr = PetscRandomCreate(PETSC_COMM_WORLD,&rctx); CHKERRQ(ierr);
-  ierr = PetscRandomSetFromOptions(rctx);
-
   /* build the matrix F in ctx */
   ierr = MatCreate(PETSC_COMM_WORLD, &(ctx->F)); CHKERRQ(ierr);
   ierr = MatSetSizes(ctx->F,PETSC_DECIDE, PETSC_DECIDE, ctx->m, ctx->n);CHKERRQ(ierr);
-  ierr = MatSetType(ctx->F,MATDUMMY); CHKERRQ(ierr); /* TODO: Decide specific SetType other than dummy*/
+  ierr = MatSetType(ctx->F,MATAIJ); CHKERRQ(ierr); /* TODO: Decide specific SetType other than dummy*/
   ierr = MatMPIAIJSetPreallocation(ctx->F, 5, NULL, 5, NULL); CHKERRQ(ierr); /*TODO: some number other than 5?*/
   ierr = MatSeqAIJSetPreallocation(ctx->F, 5, NULL); CHKERRQ(ierr);
   ierr = MatSetUp(ctx->F); CHKERRQ(ierr);
@@ -67,19 +76,34 @@ int main (int argc, char** argv)
   ierr= PetscLogStagePush(stage); CHKERRQ(ierr);
 
   /* Set matrix elements in  2-D five-point stencil format. See ksp ex02 */
-  
   if (!(ctx->matops)){
+    PetscInt gridN;
+    if (ctx->m != ctx->n) SETERRQ(PETSC_COMM_WORLD, PETSC_ERR_ARG_SIZ, "Stencil matrix must be square");
+
+    gridN = (PetscInt) PetscSqrtReal((PetscReal) ctx->m);
+    if (gridN * gridN != ctx->m) SETERRQ(PETSC_COMM_WORLD, PETSC_ERR_ARG_SIZ, "Number of rows must be square");
     for (Ii=Istart; Ii<Iend; Ii++) {
-      v = -1.0; i = Ii/ctx->n; j = Ii - i*ctx->n;
-      if (i>0)   {J = Ii - ctx->n; MatSetValues(ctx->F,1,&Ii,1,&J,&v,ADD_VALUES);}
-      if (i<ctx->m-1) {J = Ii + ctx->n; MatSetValues(ctx->F,1,&Ii,1,&J,&v,ADD_VALUES);}
-      if (j>0)   {J = Ii - 1; MatSetValues(ctx->F,1,&Ii,1,&J,&v,ADD_VALUES);}
-      if (j<ctx->n-1) {J = Ii + 1; MatSetValues(ctx->F,1,&Ii,1,&J,&v,ADD_VALUES);}
-      v = 4.0; MatSetValues(ctx->F,1,&Ii,1,&Ii,&v,ADD_VALUES);
+      PetscInt I_n, I_s, I_e, I_w;
+      v = -1.0; i = Ii / gridN; j = Ii % gridN;
+
+      I_n = i * gridN + j + 1;
+      if (j + 1 >= gridN) I_n = -1;
+      I_s = i * gridN + j - 1;
+      if (j - 1 < 0) I_s = -1;
+      I_e = (i + 1) * gridN + j;
+      if (i + 1 >= gridN) I_e = -1;
+      I_w = (i - 1) * gridN + j;
+      if (i - 1 < 0) I_w = -1;
+
+      ierr = MatSetValue(ctx->F, Ii, Ii, 4., INSERT_VALUES);CHKERRQ(ierr);
+      ierr = MatSetValue(ctx->F, Ii, I_n, -1., INSERT_VALUES);CHKERRQ(ierr);
+      ierr = MatSetValue(ctx->F, Ii, I_s, -1., INSERT_VALUES);CHKERRQ(ierr);
+      ierr = MatSetValue(ctx->F, Ii, I_e, -1., INSERT_VALUES);CHKERRQ(ierr);
+      ierr = MatSetValue(ctx->F, Ii, I_w, -1., INSERT_VALUES);CHKERRQ(ierr);
     }
   }
   else {
-    ierr = MatSetRandom(ctx->F, rctx); CHKERRQ(ierr);
+    ierr = MatSetRandom(ctx->F, ctx->rctx); CHKERRQ(ierr);
 
   }
   ierr = MatAssemblyBegin(ctx->F, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
@@ -98,9 +122,7 @@ int main (int argc, char** argv)
   ierr = VecCreate(PETSC_COMM_WORLD,&(ctx->d)); CHKERRQ(ierr);
   ierr = VecSetSizes(ctx->d,PETSC_DECIDE,ctx->m); CHKERRQ(ierr);
   ierr=  VecSetFromOptions(ctx->d); CHKERRQ(ierr);
-  
-  ierr = VecSetRandom(ctx->d,rctx); CHKERRQ(ierr);
-  ierr = PetscRandomDestroy(&rctx); CHKERRQ(ierr);
+  ierr = VecSetRandom(ctx->d,ctx->rctx); CHKERRQ(ierr);
 
   /* Define a single function that calls both components adds them together: the complete objective,
    * in the absence of a Tao implementation that handles separability */
@@ -112,7 +134,9 @@ int main (int argc, char** argv)
   /* examine solution */
   ierr = MatDestroy(&(ctx->F)); CHKERRQ(ierr);
   ierr = VecDestroy(&(ctx->d)); CHKERRQ(ierr);
-  ierr = PetscFree(ctx);CHKERRQ(ierr);
+
+  /* cleanup */
+  ierr = DestroyContext(&ctx);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
 }
