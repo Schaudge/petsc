710e706f4ea1d78829c02e819b063b57001191b2
Dave May <dave.mayhem23@gmail.com>
2019-03-06 20:37:31 +0000

    Added include/petsc/private/petscsystypes.h * This includes all implementation definitions * The reason this is required is so that the macro PetscPointerGetPetscHeaderType() can correctly compute the sizeof a struct used by a PETSc implementation (e.g. SNES). * For the moment, I am not sure how to deal with the C++ moab implementation (dmmbimpl.h) so it has been excluded from this list.

---
+++ include/petsc/private/petscsystypes.h
@@ -0,0 +1,73 @@
+
+/*
+  This file includes all private implementation headers listed in this directory
+*/
+
+#if !defined(__PRIVATE_PETSC_TYPE_IMPL_ALL)
+#define __PRIVATE_PETSC_TYPE_IMPL_ALL
+
+/*#include "../../petscsystypes.h"*/
+
+#include "bagimpl.h"
+#include "containerimpl.h"
+#include "dmproductimpl.h"
+#include "hashset.h"
+#include "pcgamgimpl.h"
+#include "taoimpl.h"
+#include "characteristicimpl.h"
+#include "dmstagimpl.h"
+#include "hashseti.h"
+#include "pcimpl.h"
+#include "taolinesearchimpl.h"
+#include "dmadaptorimpl.h"
+#include "dmswarmimpl.h"
+#include "hashsetij.h"
+#include "pcmgimpl.h"
+#include "tshistoryimpl.h"
+#include "dmdaimpl.h"
+#include "drawimpl.h"
+#include "hashtable.h"
+#include "pcpatchimpl.h"
+#include "tsimpl.h"
+#include "dmfieldimpl.h"
+#include "dtimpl.h"
+#include "isimpl.h"
+#include "petscconvestimpl.h"
+#include "vecimpl.h"
+#include "dmforestimpl.h"
+#include "f90impl.h"
+#include "petscdsimpl.h"
+#include "vecscatterimpl.h"
+#include "dmimpl.h"
+#include "fortranimpl.h"
+#include "kspimpl.h"
+#include "petscfeimpl.h"
+#if defined(PETSC_HAVE_ADIOS2)
+#include "vieweradios2impl.h"
+#endif
+#include "dmlabelimpl.h"
+#include "glvisvecimpl.h"
+#include "linesearchimpl.h"
+#include "petscfptimpl.h"
+#if defined(PETSC_HAVE_ADIOS)
+#include "vieweradiosimpl.h"
+#endif
+/*#include "dmmbimpl.h"*/
+#include "glvisviewerimpl.h"
+#include "logimpl.h"
+#include "petscfvimpl.h"
+#include "viewerimpl.h"
+#include "dmnetworkimpl.h"
+#include "hashmap.h"
+#include "petscimpl.h"
+#include "dmpatchimpl.h"
+#include "hashmapi.h"
+#include "matimpl.h"
+#include "sfimpl.h"
+#include "dmpleximpl.h"
+#include "hashmapij.h"
+#include "matorderimpl.h"
+#include "snesimpl.h"
+
+#endif
+
