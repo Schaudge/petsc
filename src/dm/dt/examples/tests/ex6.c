d63471694f5a83f431102f17b3d8e46eae3bb15a
hannah_mairs <hannah.mairs@gmail.com>
2019-03-20 10:44:21 -0500

    Fixing Barry comments

--- src/dm/dt/examples/tests/ex6.c
+++ src/dm/dt/examples/tests/ex6.c
@@ -3,7 +3,7 @@ static char help[] = "Tests 1D Gauss-Lobatto-Legendre discretization on [-1, 1].
 #include <petscdt.h>
 #include <petscviewer.h>
 
-int main(int argc,char **args)
+int main(int argc,char **argv)
 {
   PetscErrorCode ierr;
   
@@ -12,11 +12,11 @@ int main(int argc,char **args)
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
-  ierr = PetscDTGaussLobattoLegendreQuadrature(n,PETSCGLL_VIA_LINEARALGEBRA,la_nodes,la_weights);
+  ierr = PetscDTGaussLobattoLegendreQuadrature(n,PETSCGaussLobattoLegendre_VIA_LINEARALGEBRA,la_nodes,la_weights);
   ierr = PetscViewerASCIIPrintf(PETSC_VIEWER_STDOUT_SELF,"Gauss-Lobatto-Legendre nodes and weights computed via linear algebra: \n");CHKERRQ(ierr);
   ierr = PetscRealView(n,la_nodes,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
   ierr = PetscRealView(n,la_weights,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
-  ierr = PetscDTGaussLobattoLegendreQuadrature(n,PETSCGLL_VIA_NEWTON,n_nodes,n_weights);CHKERRQ(ierr);
+  ierr = PetscDTGaussLobattoLegendreQuadrature(n,PETSCGaussLobattoLegendre_VIA_NEWTON,n_nodes,n_weights);CHKERRQ(ierr);
   ierr = PetscViewerASCIIPrintf(PETSC_VIEWER_STDOUT_SELF,"Gauss-Lobatto-Legendre nodes and weights computed via Newton: \n");CHKERRQ(ierr);
   ierr = PetscRealView(n,n_nodes,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
   ierr = PetscRealView(n,n_weights,PETSC_VIEWER_STDOUT_SELF);CHKERRQ(ierr);
