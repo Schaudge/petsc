ca7a26e4cd155582848015680d1fd9669234eee2
Barry Smith <bsmith@mcs.anl.gov>
2019-05-20 16:27:55 -0500

    WIP
    Commit-type: i.e. bug-fix, portability-fix, testing-fix, style-fix, feature, documentation, example
    Funded-by:
    Project:
    Time:     hours
    Reported-by:
    Thanks-to:
    Development Tools: Vim, Emacs, Eclipse

--- src/dm/examples/tests/ex53.c
+++ src/dm/examples/tests/ex53.c
@@ -6,21 +6,23 @@ static char help[] = "Tests FAIJ matrices.\n\n";
 #include <petscdmda.h>
 
 PetscErrorCode FillUpMatrix(DM,Mat);
+PetscErrorCode FillUpMatrixSmall(DM,Mat);
 
 int main(int argc,char **argv)
 {
   PetscErrorCode ierr;
   DM             da;
-  PetscInt       dof = 2;
+  PetscInt       dof = 100,M = 100000;
   Vec            x,y;
   Mat            A;
   PetscBool      view = PETSC_FALSE;
-  PetscLogStage  stage;
+  PetscInt       i;
+  PetscLogStage  stage,stagesmall;
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
   ierr = PetscOptionsGetInt(NULL,NULL,"-dof",&dof,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetBool(NULL,NULL,"-view",&view,NULL);CHKERRQ(ierr);
-  ierr = DMDACreate1d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,7,dof,1,NULL,&da);CHKERRQ(ierr);
+  ierr = DMDACreate1d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,M,dof,1,NULL,&da);CHKERRQ(ierr);
   ierr = DMSetFromOptions(da);CHKERRQ(ierr);
   ierr = DMSetMatType(da,MATSEQFAIJ);CHKERRQ(ierr);
   ierr = DMSetUp(da);CHKERRQ(ierr);
@@ -35,7 +37,9 @@ int main(int argc,char **argv)
   ierr = VecSet(x,1.0);CHKERRQ(ierr);
   ierr = PetscLogStageRegister("Large block diagonal with efficient indexing",&stage);CHKERRQ(ierr);
   ierr = PetscLogStagePush(stage);CHKERRQ(ierr);
-  ierr = MatMult(A,x,y);CHKERRQ(ierr);
+  for (i=0; i<10; i++) {
+    ierr = MatMult(A,x,y);CHKERRQ(ierr);
+  }
   ierr = PetscLogStagePop();CHKERRQ(ierr);
   if (view) {
     ierr = VecView(y,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
@@ -45,6 +49,36 @@ int main(int argc,char **argv)
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = VecDestroy(&y);CHKERRQ(ierr);
   ierr = DMDestroy(&da);CHKERRQ(ierr);
+
+  /* ------------------------------------------------------------------------*/
+  ierr = DMDACreate1d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,M,1,1,NULL,&da);CHKERRQ(ierr);
+  ierr = DMSetFromOptions(da);CHKERRQ(ierr);
+  ierr = DMSetMatType(da,MATSEQFAIJ);CHKERRQ(ierr);
+  ierr = DMSetUp(da);CHKERRQ(ierr);
+  ierr = DMCreateMatrix(da,&A);CHKERRQ(ierr);
+  ierr = FillUpMatrixSmall(da,A);CHKERRQ(ierr);
+  if (view) {
+    ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  }
+  ierr = DMCreateGlobalVector(da,&x);CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(da,&y);CHKERRQ(ierr);
+
+  ierr = VecSet(x,1.0);CHKERRQ(ierr);
+  ierr = PetscLogStageRegister("Small matrix with efficient indexing",&stagesmall);CHKERRQ(ierr);
+  ierr = PetscLogStagePush(stage);CHKERRQ(ierr);
+  for (i=0; i<10*dof; i++) {
+    ierr = MatMult(A,x,y);CHKERRQ(ierr);
+  }
+  ierr = PetscLogStagePop();CHKERRQ(ierr);
+  if (view) {
+    ierr = VecView(y,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  }
+
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = VecDestroy(&x);CHKERRQ(ierr);
+  ierr = VecDestroy(&y);CHKERRQ(ierr);
+  ierr = DMDestroy(&da);CHKERRQ(ierr);
+
   ierr = PetscFinalize();
   return ierr;
 }
@@ -77,6 +111,37 @@ PetscErrorCode FillUpMatrix(DM da,Mat A)
     }
   }
   ierr = PetscFree(v);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode FillUpMatrixSmall(DM da,Mat A)
+{
+  PetscErrorCode ierr;
+  PetscInt       mx,xs,xm,i,col[3];
+  PetscReal      h;
+  PetscScalar    v[3];
+
+  PetscFunctionBeginUser;
+  ierr = DMDAGetInfo(da,0,&mx,0,0,0,0,0,0,0,0,0,0,0);CHKERRQ(ierr);
+  ierr = DMDAGetCorners(da,&xs,0,0,&xm,0,0);CHKERRQ(ierr);
+  h    = 1.0/(mx-1);
+
+  for (i=xs; i<xs+xm; i++) {
+    if (i==0 || i==mx-1) {
+      v[0] = 2.0/h;
+      ierr = MatSetValuesBlocked(A,1,&i,1,&i,v,INSERT_VALUES);CHKERRQ(ierr);
+    } else {
+      v[0] = -1.0/h;
+      col[0] = i-1;
+      v[1] = 2.0/h;
+      col[1] = i;
+      v[2] = -1.0/h;
+      col[2] = i+1;
+      ierr  = MatSetValuesBlocked(A,1,&i,3,col,v,INSERT_VALUES);CHKERRQ(ierr);
+    }
+  }
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   PetscFunctionReturn(0);
