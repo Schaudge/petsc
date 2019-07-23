3c8689081edd9e6a9d50bb3d4a5016d48dbb5c36
jfaibussowitsch <jfaibussowitscg@anl.gov>
2019-07-23 15:32:59 +0000

    Adding shell scripts to tracking

---
+++ src/ts/examples/tutorials/multirun.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+
+#COBALT -t 180
+#COBALT -q default
+#COBALT --attrs mcdram=cache:numa=quad
+#COBALT -A $COBALT_PROJDM
+
+echo "Start"
+echo "-n total mpi ranks (-N*-n)"
+echo "-N ranks per node"
+echo "-cc how ranks are bound to cores, depth allows specification"
+echo "-d hardware threads per rank"
+echo "-j hardware threads per core (max 4)"
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+++                   DEPLOYED VERSION                    +++"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "Creating Directories"
+now=$(date +"%m_%d_%Y")
+dirname="out_dep_$now"
+mkdir -p ./$dirname
+filtINSERT="./${dirname}/filtoutINSERT.txt"
+filtADDVAL="./${dirname}/filtoutADDVAL.txt"
+nprocess="./${dirname}/nprocess.txt"
+rawlog="./${dirname}/rawlog.txt"
+cellprank="./${dirname}/cellprank.txt"
+packsizeINSERT="./${dirname}/packsizeINSERT.txt"
+packsizeADDVAL="./${dirname}/packsizeADDVAL.txt"
+
+echo "resetting full log and filtered outputs..."
+>./$filtINSERT
+>./$filtADDVAL
+>./$nprocess
+>./$rawlog
+>./$cellprank
+>./$packsizeINSERT
+>./$packsizeADDVAL
+echo "done"
+
+maxcount=18
+counter=11
+cells=8000000
+echo "Max number of iterations:		$(($maxcount-$counter))"
+echo "looping..."
+echo "-----------"
+until [ $counter -gt $maxcount ]
+do
+    ranks=$((2**$counter))
+    cellprank=$(bc <<< "scale=3; $cells/$ranks")
+    echo "counter:			   	$counter"
+    echo "current number of MPI ranks:         	$ranks"
+    echo "approx cells/rank:                    $cellprank"
+    echo "start time:                      	$(date -u)"
+    SECONDS=0
+    aprun -n $ranks -N 256 -cc depth -d 1 -j 4 ./exspeedtest -f ssthimble8M.msh4 -speed -log_view >> ./$rawlog
+    echo "+++++++++++++++++++++++++++ End of Log ++++++++++++++++++++++++++++++++++">>./$rawlog
+    ((counter++))
+    duration=$SECONDS
+    echo "end time:                      	$(date -u)"
+    echo "runtime:                         	$(($duration / 60)) minutes and $(($duration % 60)) seconds"
+    echo "$cellprank">>./$cellprank
+    echo "-----------"
+done
+echo "--------------------------- Successful exit! ---------------------------">>./rawlog.txt
+echo "done"
+echo "grepping..."
+grep "CommINSERT" --line-buffered ./$rawlog | awk '{print $4}' >> ./$filtINSERT
+grep "CommADDVAL" --line-buffered ./$rawlog | awk '{print $4}' >> ./$filtADDVAL
+grep "./exspeedtest on a" --line-buffered ./$rawlog | awk '{print $8}' >> ./$nprocess
+grep "CommINSERT" --line-buffered ./$rawlog | awk '{print $9}' >> ./$packsizeINSERT
+grep "CommADDVAL" --line-buffered ./$rawlog | awk '{print $9}' >> ./$packsizeADDVAL
+echo "done"
---
+++ src/ts/examples/tutorials/multirundebug.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+
+#COBALT -t 60
+#COBALT -q debug-flat-quad
+#COBALT --attrs mcdram=cache:numa=quad
+#COBALT -A $COBALT_PROJ
+
+echo "Start"
+echo "-n total mpi ranks (-N*-n)"
+echo "-N ranks per node"
+echo "-cc how ranks are bound to cores, depth allows specification"
+echo "-d hardware threads per rank"
+echo "-j hardware threads per core (max 4)"
+echo "aprun -n 256 -N 256 -cc depth -d 1 -j 4 ./exspeedtest -f ssthimble1M.msh4 -disp -log_view"
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+++                     DEBUG VERSION                     +++"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+echo "Creating Directories"
+now=$(date +"%m_%d_%Y")
+dirname="out_debug_$now"
+mkdir -p ./$dirname
+filtINSERT="./${dirname}/filtoutdebugINSERT.txt"
+filtADDVAL="./${dirname}/filtoutdebugADDVAL.txt"
+nprocess="./${dirname}/nprocessdebug.txt"
+rawlog="./${dirname}/rawlogdebug.txt"
+cellprank="./${dirname}/cellprankdebug.txt"
+packsizeINSERT="./${dirname}/packsizedebugINSERT.txt"
+packsizeADDVAL="./${dirname}/packsizedebugADDVAL.txt"
+
+echo "resetting full log and filtered outputs..."
+>./$filtINSERT
+>./$filtADDVAL
+>./$nprocess
+>./$rawlog
+>./$cellprank
+>./$packsizeINSERT
+>./$packsizeADDVAL
+echo "done"
+
+maxcount=9
+counter=0
+cells=1000000
+echo "Max number of Iterations:		$(($maxcount-$counter))"
+echo "looping..."
+echo "-----------"
+until [ $counter -gt $maxcount ]
+do
+    #ranks=$((2**$counter))
+    ranks=256
+    cellprank=$(bc <<< "scale=3; $cells/$ranks")
+    echo "counter:                         	$counter"
+    echo "current number of MPI ranks:     	$ranks"
+    echo "approx cells/rank:			$cellprank"
+    echo "start time:                      	$(date -u)"
+    SECONDS=0
+    aprun -n $ranks -N 256 -cc depth -d 1 -j 4 ./exspeedtest -f ssthimble1M.med -speed -log_view >> ./$rawlog
+    echo "+++++++++++++++++++++++++++++ End of Log +++++++++++++++++++++++++++++++">>./$rawlog
+    ((counter++))
+    duration=$SECONDS
+    echo "end time:			 	$(date -u)"
+    echo "runtime:                         	$(($duration / 60)) minutes and $(($duration % 60)) seconds"
+    echo "$cellprank">>./$cellprank
+    echo "-----------"
+done
+echo "--------------------------- Successful exit! ---------------------------">>./$rawlog
+echo "done"
+echo "grepping..."
+grep "CommINSERT" --line-buffered ./$rawlog | awk '{print $4}' >> ./$filtINSERT
+grep "CommADDVAL" --line-buffered ./$rawlog | awk '{print $4}' >> ./$filtADDVAL
+grep "./exspeedtest on a" --line-buffered ./$rawlog | awk '{print $8}' >> ./$nprocess
+grep "CommINSERT" --line-buffered ./$rawlog | awk '{print $9}' >> ./$packsizeINSERT
+grep "CommADDVAL" --line-buffered ./$rawlog | awk '{print $9}' >> ./$packsizeADDVAL
+echo "done"
