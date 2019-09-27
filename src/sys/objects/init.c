84e6b7361e971719a96680a11243c022f25e0dd2
Stefano Zampini <stefano.zampini@gmail.com>
2019-09-27 17:35:39 +0300

    Yet another suggestion from Barry

--- src/sys/objects/init.c
+++ src/sys/objects/init.c
@@ -780,7 +780,7 @@ PETSC_INTERN PetscErrorCode  PetscOptionsCheckInitial_Private(const char help[])
     ierr = (*PetscHelpPrintf)(comm," -options_file <file>: reads options from file\n");CHKERRQ(ierr);
     ierr = (*PetscHelpPrintf)(comm," -options_monitor: monitor options to standard output, including that set previously e.g. in option files\n");CHKERRQ(ierr);
     ierr = (*PetscHelpPrintf)(comm," -options_monitor_cancel: cancels all hardwired option monitors\n");CHKERRQ(ierr);
-    ierr = (*PetscHelpPrintf)(comm," -petsc_sleep <int>: sleeps given seconds before running program\n");CHKERRQ(ierr);
+    ierr = (*PetscHelpPrintf)(comm," -petsc_sleep <real number>: sleeps given number of seconds before running program\n");CHKERRQ(ierr);
     ierr = (*PetscHelpPrintf)(comm,"-----------------------------------------------\n");CHKERRQ(ierr);
   }
 
