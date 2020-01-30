bae084241239be1b9dd95ff2530140e4eb0f7fd4
Richard Tran Mills <rmills@rmills.org>
2020-01-30 10:19:30 -0800

    Add ability to set minimum size for pinned host allocations for VECCUDA from command line.
    This minimum size now defaults to zero.
    Also fixes a bug: for VECSEQCUDA, this minimum size was not being initialized to a default.

--- src/vec/vec/impls/mpi/mpicuda/mpicuda.cu
+++ src/vec/vec/impls/mpi/mpicuda/mpicuda.cu
@@ -353,7 +353,12 @@ PetscErrorCode VecCreate_MPICUDA_Private(Vec vv,PetscBool alloc,PetscInt nghost,
       veccuda->GPUarray_allocated = 0;
       veccuda->hostDataRegisteredAsPageLocked = PETSC_FALSE;
       vv->offloadmask = PETSC_OFFLOAD_UNALLOCATED;
-      veccuda->minimum_bytes_pinned_memory = 134217728;
+      veccuda->minimum_bytes_pinned_memory = 0;
+
+      /* Need to parse command line for minimum size to use for pinned memory allocations on host here. */
+      ierr = PetscOptionsBegin(PetscObjectComm((PetscObject)V),((PetscObject)V)->prefix,"VECCUDA Options","Vec");CHKERRQ(ierr);
+      ierr = PetscOptionsInt("-vec_cuda_pinned_memory_min","Minimum size (in bytes) for an allocation to use pinned memory on host","VecCUDASetPinnedMemoryMin",veccuda->minimum_bytes_pinned_memory,&veccuda->minimum_bytes_pinned_memory,NULL);CHKERRQ(ierr);
+      ierr = PetscOptionsEnd();CHKERRQ(ierr);
     }
     veccuda = (Vec_CUDA*)vv->spptr;
     veccuda->GPUarray = (PetscScalar*)array;
