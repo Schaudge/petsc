1f5bd4374e5ec327ba09854411d930c3cbbf57d0
Alp Dener <alp.dener@me.com>
2020-02-04 10:18:22 -0600

    Fixed formulation error, now equivalent with LMVMBFGS

--- src/ksp/ksp/utils/lmvm/cdbfgs/cdbfgs.h
+++ src/ksp/ksp/utils/lmvm/cdbfgs/cdbfgs.h
@@ -8,7 +8,7 @@ typedef struct {
   Mat       diag_bfgs;                        /* diagonalized Hessian init */
   Mat       STfull, YTfull;                   /* large matrices (m x n) */
   Mat       StYfull, Lfull, Dfull, Rfull;     /* small matrices (m x m) */
-  Mat       ST, YT, StY, L, D, R, Rinv;       /* submatrices that span only the existing updates */
+  Mat       ST, YT, StY, L, D, R, Rinv;               /* submatrices that span only the existing updates */
   Vec       rwork1, rwork2, rwork3, rwork4;   /* small work vectors (m) matching submatrices */
   Vec       lwork1, lwork2;                   /* large work vectors (n) */
   PetscInt  watchdog, max_seq_rejects;        /* tracker to reset after a certain # of consecutive rejects */
