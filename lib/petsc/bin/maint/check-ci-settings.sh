b04a5b66f3384ae56f41a1657f52b3012a4e673a
Jed Brown <jed@jedbrown.org>
2020-02-22 12:59:13 -0700

    CI: test alternative script

--- lib/petsc/bin/maint/check-ci-settings.sh
+++ lib/petsc/bin/maint/check-ci-settings.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
-set -e
-set -x
-git fetch -q origin maint
-maint=$(git rev-parse FETCH_HEAD)
-git fetch -q origin master
-master=$(git rev-parse FETCH_HEAD)
-if [ $(git merge-base --octopus $maint $master HEAD) =  $(git merge-base $master HEAD) ]
- then dest=$maint; deststr=origin/maint
- else dest=$master; deststr=origin/master
+#!/bin/bash -ex
+
+git fetch --no-tags origin +maint:remotes/origin/maint +master:remotes/origin/master
+base_maint=$(git merge-base --octopus origin/maint origin/master HEAD)
+base_master=$(git merge-base origin/master HEAD)
+if [ ${base_maint} = ${base_master} ]; then
+    dest=origin/maint
+else
+    dest=origin/master
 fi
-if [ -z "$(git diff HEAD...$dest .gitlab-ci.yml)" ]
-  then printf "Success! Using current CI settings as in gitlab-ci.yml in $deststr!\n"
-  else printf "ERROR! Using old CI settings in gitlab-ci.yml! Please rebase to $deststr to use current CI settings.\n"; exit 1
+if git diff --exit-code HEAD...${dest} -- .gitlab-ci.yml; then
+    printf "Success! Using current CI settings as in gitlab-ci.yml in ${dest}!\n"
+else
+    printf "ERROR! Using old CI settings in gitlab-ci.yml! Please rebase to ${dest} to use current CI settings.\n"
+    exit 1
 fi
 
