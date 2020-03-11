3dc144a1321af695e0de43f2fdef7d2e42f6c316
Mark Adams <mfadams@lbl.gov>
2020-03-11 08:09:27 -0400

    clean up, working on impurity injection

--- include/petsc/private/dmpleximpl.h
+++ include/petsc/private/dmpleximpl.h
@@ -696,8 +696,6 @@ typedef struct {
   PetscReal     electronShift; /* for tests */
   /* diagnostics */
   PetscInt      verbose;
-  PetscInt      plotIdx;
-  PetscReal     plotDt;
   PetscLogEvent events[20];
   DM            dmv;
   /* cache */
