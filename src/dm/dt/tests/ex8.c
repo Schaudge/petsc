ba6cc320ab73c46964388933d883b41f9a35df3e
Brandon Michael Whitchurch <brandonwhitchurch@gmail.com>
2020-03-27 14:04:38 -0400

    Started a dm_dt_tests-ex8 for testing sumspaces.

--- src/dm/dt/tests/ex8.c
+++ src/dm/dt/tests/ex8.c
@@ -1,58 +1,33 @@
-const char help[] = "Tests PetscDTBaryToIndex(), PetscDTIndexToBary(), PetscDTIndexToGradedOrder() and PetscDTGradedOrderToIndex()";
+static char help[] = "Tests PetscSpace_Sum.\n\n";
 
-#include <petsc/private/petscimpl.h>
-#include <petsc/private/dtimpl.h>
-#include <petsc/private/petscfeimpl.h>
-
-int main(int argc, char **argv)
+#include <petscfe.h>
+int main(int argc,char **argv)
 {
-  PetscInt       d, n, maxdim = 4;
-  PetscInt       *btupprev, *btup;
-  PetscInt       *gtup;
+  PetscSpace P;
+  PetscInt Ns = 2;
+  PetscInt Nc = 1;
+  PetscInt Nv = 1;
+  PetscInt order = 2;
   PetscErrorCode ierr;
 
-  ierr = PetscInitialize(&argc, &argv, NULL, help);if (ierr) return ierr;
-  ierr = PetscMalloc3(maxdim + 1, &btup, maxdim + 1, &btupprev, maxdim, &gtup);CHKERRQ(ierr);
-  for (d = 0; d <= maxdim; d++) {
-    for (n = 0; n <= d + 2; n++) {
-      PetscInt j, k, Nk, kchk;
+  ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
+  ierr = PetscSpaceCreate(PETSC_COMM_WORLD, &P); CHKERRQ(ierr);
+  ierr = PetscSpaceSetType(P, PETSCSPACESUM); CHKERRQ(ierr);
+  ierr = PetscSpaceSumSetNumSubspaces(P, Ns); CHKERRQ(ierr);
+  ierr = PetscSpaceSetNumComponents(P, Nc); CHKERRQ(ierr);
+  ierr = PetscSpaceSetNumVariables(P,Nv);CHKERRQ(ierr);
+  ierr = PetscSpaceSetDegree(P, order, order);CHKERRQ(ierr);
+  ierr = PetscSpaceSetUp(P);CHKERRQ(ierr);
 
-      ierr = PetscDTBinomialInt(d + n, d, &Nk);CHKERRQ(ierr);
-      for (k = 0; k < Nk; k++) {
-        PetscInt sum;
+  ierr = PetscSpaceView(P, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
-        ierr = PetscDTIndexToBary(d + 1, n, k, btup);CHKERRQ(ierr);
-        for (j = 0, sum = 0; j < d + 1; j++) {
-          if (btup[j] < 0) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "PetscDTIndexToBary, d = %D, n = %D, k = %D negative entry\n", d, n, k);
-          sum += btup[j];
-        }
-        if (sum != n) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "PetscDTIndexToBary, d = %D, n = %D, k = %D incorrect sum\n", d, n, k);
-        ierr = PetscDTBaryToIndex(d + 1, n, btup, &kchk);CHKERRQ(ierr);
-        if (kchk != k) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "PetscDTBaryToIndex, d = %D, n = %D, k = %D mismatch\n", d, n, k);
-        if (k) {
-          j = d;
-          while (j >= 0 && btup[j] == btupprev[j]) j--;
-          if (j < 0) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "PetscDTIndexToBary, d = %D, n = %D, k = %D equal to previous\n", d, n, k);
-          if (btup[j] < btupprev[j]) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "PetscDTIndexToBary, d = %D, n = %D, k = %D less to previous\n", d, n, k);
-        } else {
-          ierr = PetscArraycpy(btupprev, btup, d + 1);CHKERRQ(ierr);
-        }
-        ierr = PetscDTIndexToGradedOrder(d, Nk - 1 - k, gtup);CHKERRQ(ierr);
-        ierr = PetscDTGradedOrderToIndex(d, gtup, &kchk);CHKERRQ(ierr);
-        if (kchk != Nk - 1 - k) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "PetscDTGradedOrderToIndex, d = %D, n = %D, k = %D mismatch\n", d, n, Nk - 1 - k);
-        for (j = 0; j < d; j++) {
-          if (gtup[j] != btup[d - 1 - j]) SETERRQ3(PETSC_COMM_SELF, PETSC_ERR_PLIB, "PetscDTIndexToGradedOrder, d = %D, n = %D, k = %D incorrect\n", d, n, Nk - 1 - k);
-        }
-      }
-    }
-  }
-  ierr = PetscFree3(btup, btupprev, gtup);CHKERRQ(ierr);
+  PetscSpaceDestroy(&P);
   ierr = PetscFinalize();
   return ierr;
 }
 
 /*TEST
-
   test:
-
+    suffix: 0
+    args: -malloc_dump
 TEST*/
