86e1b145f245aa3559be2f646ddf359aca9331e9
Jed Brown <jed@jedbrown.org>
2020-04-13 21:03:41 -0600

    DMPlexCreateGyroidMesh: add -periodic option

--- src/dm/impls/plex/tutorials/ex10.c
+++ src/dm/impls/plex/tutorials/ex10.c
@@ -7,18 +7,20 @@ int main(int argc, char **argv)
   DM             dm;
   PetscInt       extent[3] = {1,1,1}, refine = 0, three;
   PetscBool      gyroid = PETSC_FALSE;
+  DMBoundaryType periodic[3] = {DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE};
   PetscErrorCode ierr;
 
   ierr = PetscInitialize(&argc, &argv, NULL,help);if (ierr) return ierr;
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD, NULL, "Schwarz P Example", NULL);CHKERRQ(ierr);
   ierr = PetscOptionsIntArray("-extent", "Number of replicas for each of three dimensions", NULL, extent, (three=3, &three), NULL);CHKERRQ(ierr);
   ierr = PetscOptionsInt("-refine", "Number of refinements", NULL, refine, &refine, NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsEnumArray("-periodic", "Periodicity in each of three dimensions", NULL, DMBoundaryTypes, (PetscEnum*)periodic, (three=3, &three), NULL);CHKERRQ(ierr);
   ierr = PetscOptionsBool("-gyroid", "Use the gyroid triply periodic surface instead", NULL, gyroid, &gyroid, NULL);CHKERRQ(ierr);
   ierr = PetscOptionsEnd();CHKERRQ(ierr);
   if (!gyroid) {
     ierr = DMPlexCreateTPSMesh(PETSC_COMM_WORLD, extent, 0, refine, &dm);CHKERRQ(ierr);
   } else {
-    ierr = DMPlexCreateGyroidMesh(PETSC_COMM_WORLD, extent, NULL, 0, refine, &dm);CHKERRQ(ierr);
+    ierr = DMPlexCreateGyroidMesh(PETSC_COMM_WORLD, extent, periodic, 0, refine, &dm);CHKERRQ(ierr);
   }
   ierr = DMViewFromOptions(dm, NULL, "-dm_view");CHKERRQ(ierr);
   ierr = DMDestroy(&dm);CHKERRQ(ierr);
