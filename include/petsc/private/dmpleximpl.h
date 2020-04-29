6b54efc68d7b3eb2d8bb47497e75bb38f10e6fa0
Mark Adams <ma2325@columbia.edu>
2020-04-29 17:11:13 -0400

    added removal of drift from T, change convergence criterion

--- include/petsc/private/dmpleximpl.h
+++ include/petsc/private/dmpleximpl.h
@@ -622,7 +622,7 @@ PETSC_INTERN PetscErrorCode DMCreateNeumannOverlap_Plex(DM, IS*, Mat*, PetscErro
 #define FP_MAX_NQ 27 /* Q2/Q4 */
 #endif
 #if !defined(FP_MAX_SUB_THREAD_BLOCKS)
-#define FP_MAX_SUB_THREAD_BLOCKS 8
+#define FP_MAX_SUB_THREAD_BLOCKS 32
 #endif
 typedef struct {
   PetscBool     interpolate;                  /* Generate intermediate mesh elements */
