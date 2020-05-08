3f30c9e673965b8285d014f19ba37b0b8d30e661
Martin Diehl <mail@martin-diehl.net>
2020-05-08 09:36:31 +0200

    variables from taolinesearch

--- src/tao/f90-mod/petsctao.h
+++ src/tao/f90-mod/petsctao.h
@@ -1,4 +1,16 @@
 #include "petsc/finclude/petsctao.h"
+      
+      type tTao
+        sequence
+        PetscFortranAddr :: v PETSC_FORTRAN_TYPE_INITIALIZE
+      end type tTao
+      type tTaoLineSearch
+        sequence
+        PetscFortranAddr :: v PETSC_FORTRAN_TYPE_INITIALIZE
+      end type tTaoLineSearch
+
+      Tao, parameter :: PETSC_NULL_TAO = tTao(0)
+      TaoLineSearch, parameter :: PETSC_NULL_TAOLINESEARCH = tTaoLineSearch(0)
 
       ! TaoSubsetType
       PetscEnum, parameter :: TAO_SUBSET_SUBVEC = 0
@@ -28,3 +40,17 @@
       PetscEnum, parameter :: TAO_DIVERGED_TR_REDUCTION = -7
       PetscEnum, parameter :: TAO_DIVERGED_USER = -8
       PetscEnum, parameter :: TAO_CONTINUE_ITERATING = 0
+
+      ! TaoLineSearchConvergedReason
+      PetscEnum, parameter :: TAOLINESEARCH_FAILED_INFORNAN = -1
+      PetscEnum, parameter :: TAOLINESEARCH_FAILED_BADPARAMETER = -2
+      PetscEnum, parameter :: TAOLINESEARCH_FAILED_ASCENT = -3
+      PetscEnum, parameter :: TAOLINESEARCH_CONTINUE_ITERATING = 0
+      PetscEnum, parameter :: TAOLINESEARCH_SUCCESS = 1
+      PetscEnum, parameter :: TAOLINESEARCH_SUCCESS_USER = 2
+      PetscEnum, parameter :: TAOLINESEARCH_HALTED_OTHER = 3
+      PetscEnum, parameter :: TAOLINESEARCH_HALTED_MAXFCN = 4
+      PetscEnum, parameter :: TAOLINESEARCH_HALTED_UPPERBOUND = 5
+      PetscEnum, parameter :: TAOLINESEARCH_HALTED_LOWERBOUND = 6
+      PetscEnum, parameter :: TAOLINESEARCH_HALTED_RTOL = 7
+      PetscEnum, parameter :: TAOLINESEARCH_HALTED_USER = 8
