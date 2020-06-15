b42757ac054b81d324a8e36c84d4da25f9b1e8d5
Aidan Hamilton <aidan@udel.edu>
2020-06-15 13:10:56 -0400

    Added Shallow water equations to the top menu. Detailed description of options still required.

--- src/ts/tutorials/multirate/finitevolume1d.h
+++ src/ts/tutorials/multirate/finitevolume1d.h
@@ -32,9 +32,12 @@ void Limit_CadaTorrilhon3R100(LimitInfo,const PetscScalar*,const PetscScalar*,Pe
 
 typedef enum {FVBC_PERIODIC, FVBC_OUTFLOW, FVBC_INFLOW} FVBCType;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 typedef enum {NONE, TOPOGRAPHY} SOURCEType; 
 >>>>>>> Added Support for Inflow Boundary Conditions
+=======
+>>>>>>> Added Shallow water equations to the top menu. Detailed description of options still required.
 extern const char *FVBCTypes[];
 /* we add three new variables at the end of input parameters of function to be position of cell center, left bounday of domain, right boundary of domain */
 typedef PetscErrorCode (*RiemannFunction)(void*,PetscInt,const PetscScalar*,const PetscScalar*,PetscScalar*,PetscReal*,PetscReal,PetscReal,PetscReal);
@@ -92,11 +95,15 @@ typedef struct {
   PetscInt                      dof;
   char                          *fieldname[16];
 <<<<<<< HEAD
+<<<<<<< HEAD
   PetscBool                     *bcinflowindex;   /* Boolean array where bcinflowindex[dof*i+j] = TRUE indicates that the jth component of the solution
                                                      is an inflow boundary condition and i = 0 is left bc, i = 1 is right bc. FALSE implies outflow
                                                      outflow boundary condition. */
 =======
   SOURCEType                    srctype;
+=======
+  PetscBool                     issource; 
+>>>>>>> Added Shallow water equations to the top menu. Detailed description of options still required.
   PetscBool                     *bcinflowindex;   /* Boolean array where bcinflowindex[dof*i+j] = TRUE indicates that the jth component of the solution
                                    is an inflow boundary condition and i = 0 is left bc, i = 1 is right bc. FALSE implies outflow 
                                    outflow boundary condition.*/
