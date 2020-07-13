3c0243c71215658ff6a58a5a61899bdcd4d03518
Mark Adams <mfadams@lbl.gov>
2020-07-13 09:52:24 -0400

    updated arg prefixes

--- src/dm/impls/plex/landau/tutorials/ex1f90.F90
+++ src/dm/impls/plex/landau/tutorials/ex1f90.F90
@@ -51,10 +51,10 @@ program DMPlexTestLandInterface
   !    Create timestepping solver context
   ! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   call TSCreate(PETSC_COMM_SELF,ts,ierr);CHKERRA(ierr)
-  call TSSetOptionsPrefix(ts, 'land_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
+  call TSSetOptionsPrefix(ts, 'ex1f90_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
   call TSSetDM(ts,dm,ierr);CHKERRA(ierr)
   call TSGetSNES(ts,snes,ierr);CHKERRA(ierr)
-  call SNESSetOptionsPrefix(snes, 'land_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
+  call SNESSetOptionsPrefix(snes, 'ex1f90_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
   call SNESGetLineSearch(snes,linesearch,ierr);CHKERRA(ierr)
   call SNESLineSearchSetType(linesearch,SNESLINESEARCHBASIC,ierr);CHKERRA(ierr)
   call TSSetIFunction(ts,PETSC_NULL_VEC,LandIFunction,PETSC_NULL_VEC,ierr);CHKERRA(ierr)
@@ -62,9 +62,9 @@ program DMPlexTestLandInterface
   call TSSetExactFinalTime(ts,TS_EXACTFINALTIME_STEPOVER,ierr);CHKERRA(ierr)
 
   call SNESGetKSP(snes,ksp,ierr);CHKERRA(ierr)
-  call KSPSetOptionsPrefix(ksp, 'land_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
+  call KSPSetOptionsPrefix(ksp, 'ex1f90_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
   call KSPGetPC(ksp,pc,ierr);CHKERRA(ierr)
-  call PCSetOptionsPrefix(pc, 'land_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
+  call PCSetOptionsPrefix(pc, 'ex1f90_', ierr);CHKERRA(ierr) ! should get this from the dm or give it to the dm
 
   call TSSetFromOptions(ts,ierr);CHKERRA(ierr)
   call TSSetSolution(ts,X,ierr);CHKERRA(ierr)
@@ -95,6 +95,6 @@ end program DMPlexTestLandInterface
 !  test:
 !    suffix: 0
 !    requires: p4est !complex
-!    args: -petscspace_degree 4 -petscspace_poly_tensor 1 -dm_type p4est -info :dm,tsadapt -ion_masses 2,4 -ion_charges 1,8 -thermal_temps 5,5,.5 -n 1.00018,1,1e-5 -n_0 1e20 -land_ts_monitor -land_snes_rtol 1.e-6 -land_snes_monitor -land_snes_converged_reason -land_ts_type arkimex -land_ts_arkimex_type 1bee -land_ts_max_snes_failures -1 -land_ts_rtol 1e-4 -land_ts_dt 1.e-6 -land_ts_max_time 1 -land_ts_adapt_clip .5,1.25 -land_ts_adapt_scale_solve_failed 0.75 -land_ts_adapt_time_step_increase_delay 5 -land_ts_max_steps 1 -land_pc_type lu -land_ksp_type preonly -amr_levels_max 7 -domain_radius 5 -amr_re_levels 0 -re_radius 1 -amr_z_refine1 1 -amr_z_refine2 0 -amr_post_refine 0 -z_radius1 .1 -z_radius2 .1
+!    args: -petscspace_degree 4 -petscspace_poly_tensor 1 -dm_land_type p4est -info :dm,tsadapt -dm_land_ion_masses 2,4 -dm_land_ion_charges 1,8 -dm_land_thermal_temps 5,5,.5 -dm_land_n 1.00018,1,1e-5 -dm_land_n_0 1e20 -ex1f90_ts_monitor -ex1f90_snes_rtol 1.e-6 -ex1f90_snes_monitor -ex1f90_snes_converged_reason -ex1f90_ts_type arkimex -ex1f90_ts_arkimex_type 1bee -ex1f90_ts_max_snes_failures -1 -ex1f90_ts_rtol 1e-4 -ex1f90_ts_dt 1.e-6 -ex1f90_ts_max_time 1 -ex1f90_ts_adapt_clip .5,1.25 -ex1f90_ts_adapt_scale_solve_failed 0.75 -ex1f90_ts_adapt_time_step_increase_delay 5 -ex1f90_ts_max_steps 1 -ex1f90_pc_type lu -ex1f90_ksp_type preonly -dm_land_amr_levels_max 7 -dm_land_domain_radius 5 -dm_land_amr_re_levels 0 -dm_land_re_radius 1 -dm_land_amr_z_refine1 1 -dm_land_amr_z_refine2 0 -dm_land_amr_post_refine 0 -dm_land_z_radius1 .1 -dm_land_z_radius2 .1
 !
 !TEST*/
