94921f9f262cf9903532e98822c1ff07f8e6608c
Aidan Hamilton <aidan@udel.edu>
2020-07-15 13:48:34 -0400

    Deleted examples 3 and 4, they are currently not working, I believe due to the hardcoded one sided reconstruction used at the vertices. To be fixed soon

--- src/ts/tutorials/multirate/ex9.c
+++ src/ts/tutorials/multirate/ex9.c
@@ -33,8 +33,6 @@ static const char help[] = "1D periodic Finite Volume solver in slope-limiter fo
     MRPK timestepping:
     mpiexec -np 1 ./ex9 -Mx 20 -initial 0 -subcase 1 -hratio 2 -limit minmod -ts_dt 0.1 -ts_max_time 7.0 -viewdm -ymax 3 -ymin 0 -ts_type mprk -ts_mprk_type 2a22 -ts_use_splitrhsfunction 1 -bufferwidth 4
     mpiexec -np 1 ./ex9 -Mx 20 -initial 0 -subcase 2 -hratio 2 -limit minmod -ts_dt 0.1 -ts_max_time 2.5 -viewdm -ymax 5.1 -ymin -5.1 -ts_type mprk -ts_mprk_type 2a22 -ts_use_splitrhsfunction 1 -bufferwidth 4
-    mpiexec -np 1 ./ex9 -Mx 20 -initial 0 -subcase 3 -hratio 2 -limit minmod -ts_dt 0.1 -ts_max_time 4.0 -viewdm -ymax 2 -ymin -2 -ts_type mprk -ts_mprk_type 2a22 -ts_use_splitrhsfunction 1 -bufferwidth 4
-    mpiexec -np 1 ./ex9 -Mx 20 -initial 0 -subcase 4 -hratio 2 -limit minmod -ts_dt 0.1 -ts_max_time 4.0 -viewdm -ymax 2 -ymin -2 -ts_type mprk -ts_mprk_type 2a22 -ts_use_splitrhsfunction 1 -bufferwidth 4
     mpiexec -np 1 ./ex9 -Mx 20 -initial 0 -subcase 5 -hratio 2 -limit minmod -ts_dt 0.2 -ts_max_time 5.0 -viewdm -ymax 0.5 -ymin -0.5 -ts_type mprk -ts_mprk_type 2a22 -ts_use_splitrhsfunction 1 -bufferwidth 4
 */
 
