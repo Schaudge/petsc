ce47a7c5c73d998895b82d030fd3af8f381072b1
Ed Bueler <elbueler@alaska.edu>
2020-08-31 10:45:23 -0800

    more whitespace changes to conform to petsc style

--- src/ts/tutorials/ex54.c
+++ src/ts/tutorials/ex54.c
@@ -44,59 +44,60 @@ int main(int argc,char **argv)
   PetscReal      tf,xf,yf,errnorm;
   PetscInt       steps;
 
-  ierr = PetscInitialize(&argc,&argv,NULL,help);  if (ierr) return ierr;
-  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size); CHKERRQ(ierr);
+  ierr = PetscInitialize(&argc,&argv,NULL,help);
+  if (ierr) return ierr;
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
   if (size != 1) SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_WRONG_MPI_SIZE,"Uniprocessor example only!");
 
   user.identity_in_F = PETSC_FALSE;
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD,"","Simple F(t,u,u')=G(t,u) ODE system.","TS");CHKERRQ(ierr);
   ierr = PetscOptionsBool("-identity_in_F","set up system so the dF/d(dudt) = I","ex54.c",user.identity_in_F,&(user.identity_in_F),NULL);CHKERRQ(ierr);
-  ierr = PetscOptionsEnd(); CHKERRQ(ierr);
+  ierr = PetscOptionsEnd();CHKERRQ(ierr);
 
-  ierr = VecCreate(PETSC_COMM_WORLD,&u); CHKERRQ(ierr);
-  ierr = VecSetSizes(u,PETSC_DECIDE,2); CHKERRQ(ierr);
+  ierr = VecCreate(PETSC_COMM_WORLD,&u);CHKERRQ(ierr);
+  ierr = VecSetSizes(u,PETSC_DECIDE,2);CHKERRQ(ierr);
   ierr = VecSetFromOptions(u); CHKERRQ(ierr);
 
-  ierr = MatCreate(PETSC_COMM_WORLD,&JF); CHKERRQ(ierr);
-  ierr = MatSetSizes(JF,PETSC_DECIDE,PETSC_DECIDE,2,2); CHKERRQ(ierr);
-  ierr = MatSetFromOptions(JF); CHKERRQ(ierr);
-  ierr = MatSetUp(JF); CHKERRQ(ierr);
-  ierr = MatDuplicate(JF,MAT_DO_NOT_COPY_VALUES,&JG); CHKERRQ(ierr);
+  ierr = MatCreate(PETSC_COMM_WORLD,&JF);CHKERRQ(ierr);
+  ierr = MatSetSizes(JF,PETSC_DECIDE,PETSC_DECIDE,2,2);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(JF);CHKERRQ(ierr);
+  ierr = MatSetUp(JF);CHKERRQ(ierr);
+  ierr = MatDuplicate(JF,MAT_DO_NOT_COPY_VALUES,&JG);CHKERRQ(ierr);
 
-  ierr = TSCreate(PETSC_COMM_WORLD,&ts); CHKERRQ(ierr);
-  ierr = TSSetApplicationContext(ts,&user); CHKERRQ(ierr);
+  ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
+  ierr = TSSetApplicationContext(ts,&user);CHKERRQ(ierr);
 
-  ierr = TSSetIFunction(ts,NULL,FormIFunction,&user); CHKERRQ(ierr);
-  ierr = TSSetIJacobian(ts,JF,JF,FormIJacobian,&user); CHKERRQ(ierr);
+  ierr = TSSetIFunction(ts,NULL,FormIFunction,&user);CHKERRQ(ierr);
+  ierr = TSSetIJacobian(ts,JF,JF,FormIJacobian,&user);CHKERRQ(ierr);
 
-  ierr = TSSetRHSFunction(ts,NULL,FormRHSFunction,&user); CHKERRQ(ierr);
-  ierr = TSSetRHSJacobian(ts,JG,JG,FormRHSJacobian,&user); CHKERRQ(ierr);
+  ierr = TSSetRHSFunction(ts,NULL,FormRHSFunction,&user);CHKERRQ(ierr);
+  ierr = TSSetRHSJacobian(ts,JG,JG,FormRHSJacobian,&user);CHKERRQ(ierr);
 
-  ierr = TSSetProblemType(ts,TS_NONLINEAR); CHKERRQ(ierr);
-  ierr = TSSetType(ts,TSBDF); CHKERRQ(ierr);
-  ierr = TSSetTime(ts,0.0); CHKERRQ(ierr);
-  ierr = TSSetMaxTime(ts,1.0); CHKERRQ(ierr);
-  ierr = TSSetTimeStep(ts,0.01); CHKERRQ(ierr);
-  ierr = TSSetExactFinalTime(ts,TS_EXACTFINALTIME_MATCHSTEP); CHKERRQ(ierr);
+  ierr = TSSetProblemType(ts,TS_NONLINEAR);CHKERRQ(ierr);
+  ierr = TSSetType(ts,TSBDF);CHKERRQ(ierr);
+  ierr = TSSetTime(ts,0.0);CHKERRQ(ierr);
+  ierr = TSSetMaxTime(ts,1.0);CHKERRQ(ierr);
+  ierr = TSSetTimeStep(ts,0.01);CHKERRQ(ierr);
+  ierr = TSSetExactFinalTime(ts,TS_EXACTFINALTIME_MATCHSTEP);CHKERRQ(ierr);
   ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
 
-  ierr = VecSetValue(u,0,1.0,INSERT_VALUES); CHKERRQ(ierr);
-  ierr = VecSetValue(u,1,3.0,INSERT_VALUES); CHKERRQ(ierr);
-  ierr = VecAssemblyBegin(u); CHKERRQ(ierr);
-  ierr = VecAssemblyEnd(u); CHKERRQ(ierr);
-  ierr = TSSolve(ts,u); CHKERRQ(ierr);
+  ierr = VecSetValue(u,0,1.0,INSERT_VALUES);CHKERRQ(ierr);
+  ierr = VecSetValue(u,1,3.0,INSERT_VALUES);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(u);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(u);CHKERRQ(ierr);
+  ierr = TSSolve(ts,u);CHKERRQ(ierr);
 
-  ierr = TSGetStepNumber(ts,&steps); CHKERRQ(ierr);
-  ierr = TSGetTime(ts,&tf); CHKERRQ(ierr);
+  ierr = TSGetStepNumber(ts,&steps);CHKERRQ(ierr);
+  ierr = TSGetTime(ts,&tf);CHKERRQ(ierr);
   xf = -3.0 * PetscExpReal(-3.0*tf) + 4.0 * PetscExpReal(2.0*tf);
   yf = PetscExpReal(-3.0*tf) + 2.0 * PetscExpReal(2.0*tf);
-  ierr = VecDuplicate(u,&uexact); CHKERRQ(ierr);
-  ierr = VecSetValue(uexact,0,xf,INSERT_VALUES); CHKERRQ(ierr);
-  ierr = VecSetValue(uexact,1,yf,INSERT_VALUES); CHKERRQ(ierr);
-  ierr = VecAssemblyBegin(uexact); CHKERRQ(ierr);
-  ierr = VecAssemblyEnd(uexact); CHKERRQ(ierr);
-  ierr = VecAXPY(u,-1.0,uexact); CHKERRQ(ierr); // u <- u + (-1.0) uexact
-  ierr = VecNorm(u,NORM_2,&errnorm); CHKERRQ(ierr);
+  ierr = VecDuplicate(u,&uexact);CHKERRQ(ierr);
+  ierr = VecSetValue(uexact,0,xf,INSERT_VALUES);CHKERRQ(ierr);
+  ierr = VecSetValue(uexact,1,yf,INSERT_VALUES);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(uexact);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(uexact);CHKERRQ(ierr);
+  ierr = VecAXPY(u,-1.0,uexact);CHKERRQ(ierr); // u <- u + (-1.0) uexact
+  ierr = VecNorm(u,NORM_2,&errnorm);CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_WORLD,"error norm at tf = %.6f from %D steps:  |u-u_exact| =  %.5e\n",(double)tf,steps,(double)errnorm);CHKERRQ(ierr);
 
   VecDestroy(&u);  VecDestroy(&uexact);
@@ -113,8 +114,8 @@ PetscErrorCode FormIFunction(TS ts, PetscReal t, Vec u, Vec dudt,Vec F, void *us
     PetscReal       *aF;
     PetscBool       flag = ((Ctx*)user)->identity_in_F;
 
-    ierr = VecGetArrayRead(u,&au); CHKERRQ(ierr);
-    ierr = VecGetArrayRead(dudt,&adudt); CHKERRQ(ierr);
+    ierr = VecGetArrayRead(u,&au);CHKERRQ(ierr);
+    ierr = VecGetArrayRead(dudt,&adudt);CHKERRQ(ierr);
     ierr = VecGetArray(F,&aF);
     if (flag) {
       aF[0] = adudt[0] + au[0] + 2.0 * au[1];
@@ -144,9 +145,9 @@ PetscErrorCode FormIJacobian(TS ts, PetscReal t, Vec u, Vec dudt,PetscReal a, Ma
       v[0] = a + 1.0;    v[1] = a + 2.0;
       v[2] = 3.0;        v[3] = a + 4.0;
     }
-    ierr = MatSetValues(P,2,row,2,col,v,INSERT_VALUES); CHKERRQ(ierr);
-    ierr = MatAssemblyBegin(P,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-    ierr = MatAssemblyEnd(P,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatSetValues(P,2,row,2,col,v,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = MatAssemblyBegin(P,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(P,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
     if (J != P) {
       ierr = MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
       ierr = MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
@@ -170,8 +171,8 @@ PetscErrorCode FormRHSFunction(TS ts, PetscReal t, Vec u,Vec G, void *user)
       aG[0] = au[0] + 8.0 * au[1];
       aG[1] = 4.0 * au[0] + 4.0 * au[1];
     }
-    ierr = VecRestoreArrayRead(u,&au); CHKERRQ(ierr);
-    ierr = VecRestoreArray(G,&aG); CHKERRQ(ierr);
+    ierr = VecRestoreArrayRead(u,&au);CHKERRQ(ierr);
+    ierr = VecRestoreArray(G,&aG);CHKERRQ(ierr);
     return 0;
 }
 
@@ -190,7 +191,7 @@ PetscErrorCode FormRHSJacobian(TS ts, PetscReal t, Vec u, Mat J, Mat P,void *use
       v[2] = 4.0;    v[3] = 4.0;
     }
     ierr = MatSetValues(P,2,row,2,col,v,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = MatAssemblyBegin(P,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyBegin(P,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
     ierr = MatAssemblyEnd(P,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
     if (J != P) {
         ierr = MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
