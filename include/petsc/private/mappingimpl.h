66d7202873ede424bcd048bce71d7ba9e3c78e98
Jacob Faibussowitsch <jacob.fai@gmail.com>
2020-09-18 13:41:06 -0400

    new interface with union

--- include/petsc/private/mappingimpl.h
+++ include/petsc/private/mappingimpl.h
@@ -22,6 +22,33 @@ struct _PetscMappingOps {
   PetscErrorCode (*restorevalues)(PetscMapping,PetscInt,const PetscInt*[]);
 };
 
+typedef struct _n_PetscMappingContiguous *PetscMappingContiguous;
+struct {
+  PetscInt  keyStart, keyEnd;  /* section-like start end */
+  PetscInt  keyStartGlobal;    /* offset into global key array */
+} _n_PetscMappingContiguous;
+
+typedef struct _n_PetscMappingNonContiguous *PetscMappingNonContiguous;
+struct {
+  PetscInt  *keys;             /* local key array */
+  PetscInt  *keyIndexGlobal;   /* locations of keys in global key array */
+} _n_PetscMappingNonContiguous;
+
+struct _p_PetscMapping {
+  PETSCHEADER(struct _PetscMappingOps);
+  PetscMapping map;                      /* Map object to processor */
+  PetscMapping permutation;              /* permutation of keys */
+  union {
+    PetscMappingContiguous    contig;    /* section-like use */
+    PetscMappingNonContiguous noncontig; /* multi-map-like use */
+  };
+  PetscInt     nKeysLocal;               /* always cached regardless of contig or not */
+  PetscBool    contiguous;               /* are the keys contiguous? */
+  PetscBool    sorted;
+  void         *data;                    /* impls, contains *idx */
+};
+
+#if (0)
 struct _p_PetscMapping {
   PETSCHEADER(struct _PetscMappingOps);
   PetscMapping      *maps;  /* n-recurvise map objects */
@@ -37,5 +64,6 @@ struct _p_PetscMapping {
   PetscBool         setup;
   void              *data;
 };
+#endif
 
 #endif
