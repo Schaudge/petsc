e4fb65386ed57ce9c22fe560a0ff12db0a86f63a
Hong Zhang <hzhang@mcs.anl.gov>
2020-10-02 21:29:33 -0500

    small Fortran fix?

--- src/vec/vec/tests/ex26f.F90
+++ src/vec/vec/tests/ex26f.F90
@@ -1,50 +1,52 @@
 !
-!
 !  Test VecGetSubVector()
 !  Contributed-by: Adrian Croucher <gitlab@mg.gitlab.com>
 
-program main
+      program main
 #include <petsc/finclude/petsc.h>
+      use petsc
+      implicit none
 
-  use petsc
-  implicit none
-  PetscMPIInt :: rank
-  PetscErrorCode :: ierr
-  PetscInt :: num_cells, subsize, i
-  PetscInt, parameter :: blocksize = 3, field = 0
-  Vec :: v, subv
-  IS :: index_set
-  PetscInt, allocatable :: subindices(:)
+      PetscMPIInt :: rank
+      PetscErrorCode :: ierr
+      PetscInt :: num_cells, subsize, i
+      PetscInt, parameter :: blocksize = 3, field = 0
+      Vec :: v, subv
+      IS :: index_set
+      PetscInt, allocatable :: subindices(:)
 
-  call PetscInitialize(PETSC_NULL_CHARACTER, ierr)
-  call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
+      call PetscInitialize(PETSC_NULL_CHARACTER, ierr)
+      if (ierr .ne. 0) then
+         print*,'Unable to initialize PETSc'
+         stop
+      endif
+      call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
 
-  if (rank == 0) then
-     num_cells = 1
-  else
-     num_cells = 0
-  end if
+      if (rank .eq. 0) then
+         num_cells = 1
+      else
+         num_cells = 0
+      end if
 
-  call VecCreate(PETSC_COMM_WORLD, v, ierr);CHKERRA(ierr)
-  call VecSetSizes(v, num_cells * blocksize, PETSC_DECIDE, ierr);CHKERRA(ierr)
-  call VecSetBlockSize(v, blocksize, ierr); CHKERRQ(ierr);CHKERRA(ierr)
-  call VecSetFromOptions(v, ierr);CHKERRQ(ierr)
+      call VecCreate(PETSC_COMM_WORLD, v, ierr);CHKERRA(ierr)
+      call VecSetSizes(v, num_cells * blocksize, PETSC_DECIDE, ierr);CHKERRA(ierr)
+      call VecSetBlockSize(v, blocksize, ierr); CHKERRQ(ierr);CHKERRA(ierr)
+      call VecSetFromOptions(v, ierr);CHKERRQ(ierr)
 
-  subsize = num_cells
-  allocate(subindices(0: subsize - 1))
-  subindices = [(i, i = 0, subsize - 1)] * blocksize + field
-  call ISCreateGeneral(PETSC_COMM_WORLD, subsize, subindices, &
-       PETSC_COPY_VALUES, index_set, ierr);CHKERRA(ierr)
-  deallocate(subindices)
+      subsize = num_cells
+      allocate(subindices(0: subsize - 1))
+      subindices = [(i, i = 0, subsize - 1)] * blocksize + field
+      call ISCreateGeneral(PETSC_COMM_WORLD, subsize, subindices, &
+           PETSC_COPY_VALUES, index_set, ierr);CHKERRA(ierr)
+      deallocate(subindices)
 
-  call VecGetSubVector(v, index_set, subv, ierr);CHKERRA(ierr)
-  call VecRestoreSubVector(v, index_set, subv, ierr);CHKERRA(ierr)
-  call ISDestroy(index_set, ierr); CHKERRQ(ierr);CHKERRA(ierr)
+      call VecGetSubVector(v, index_set, subv, ierr);CHKERRA(ierr)
+      call VecRestoreSubVector(v, index_set, subv, ierr);CHKERRA(ierr)
+      call ISDestroy(index_set, ierr); CHKERRQ(ierr);CHKERRA(ierr)
 
-  call VecDestroy(v, ierr);CHKERRQ(ierr)
-  call PetscFinalize(ierr);CHKERRQ(ierr)
-  end
-
+      call VecDestroy(v, ierr);CHKERRQ(ierr)
+      call PetscFinalize(ierr);
+      end
 
 !/*TEST
 !
