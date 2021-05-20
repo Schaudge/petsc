067fd0bcd81bb96198586e9982bab6f34f7eafb7
Patrick Sanan <patrick.sanan@gmail.com>
2021-05-20 09:48:56 +0200

    CI: add check for adding large files

--- lib/petsc/bin/maint/check-banned-files.sh
+++ lib/petsc/bin/maint/check-banned-files.sh
@@ -1,19 +1,6 @@
 #!/bin/bash -ex
 
-if [ ! -z "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME+x}" -a "${CI_MERGE_REQUEST_EVENT_TYPE}" != "detached" ]; then
-  echo Skipping as this is MR CI for ${CI_MERGE_REQUEST_TARGET_BRANCH_NAME} branch
-  exit 0
-fi
-
-git fetch --unshallow --no-tags origin +release:remotes/origin/release +main:remotes/origin/main
-
-base_release=$(git merge-base --octopus origin/release origin/main HEAD)
-base_main=$(git merge-base origin/main HEAD)
-if [ ${base_release} = ${base_main} ]; then
-    dest=origin/release
-else
-    dest=origin/main
-fi
+dest=$(bash lib/petsc/bin/maint/check-merge-branch.sh)
 
 # Search for and print newly-added files with banned extensions
 # If grep's exit code is zero (success), there was a match, so declare overall failure
@@ -39,5 +26,17 @@ then
   exit 1
 else
   printf -- "Success! No files with banned extensions added\n"
-  exit 0
+fi
+
+file_size_limit=1024 # KiB
+if git diff --name-only --diff-filter=A ${dest} \
+  | xargs du -k \
+  | awk -v limit=$file_size_limit '{if ($1 > limit) {print $0}}' \
+  | grep '.*' \
+  ; \
+then
+  printf -- "ERROR! File(s) larger than $file_size_limit KiB added\n"
+  exit 1
+else
+  printf -- "Success! No files larger than $file_size_limit KiB added\n"
 fi
