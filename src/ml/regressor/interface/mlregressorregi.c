15fa3b4e62d16af247534afebec4a38c9a01b640
Richard Tran Mills <rmills@rmills.org>
2021-06-07 16:37:56 -0700

    Start adding linear regressor implementation; incomplete.

--- src/ml/regressor/interface/mlregressorregi.c
+++ src/ml/regressor/interface/mlregressorregi.c
@@ -1,13 +1,15 @@
 #include <petsc/private/mlregressorimpl.h>
 
+PETSC_EXTERN PetscErrorCode MLRegressorCreate_Linear(MLRegressor);
+
 PetscErrorCode MLRegressorRegisterAll(void)
 {
-  //PetscErrorCode ierr;
+  PetscErrorCode ierr;
 
   PetscFunctionBegin;
   if (MLRegressorRegisterAllCalled) PetscFunctionReturn(0);
   MLRegressorRegisterAllCalled = PETSC_TRUE;
   // TODO: Register all of the sub-types
-  //ierr = MLRegressorRegister(MLREGRESSORLINEAR,MLRegressorCreate_Linear);CHKERRQ(ierr);  // Uncomment after MLRegressorCreate_Linear() exists!
+  ierr = MLRegressorRegister(MLREGRESSORLINEAR,MLRegressorCreate_Linear);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
