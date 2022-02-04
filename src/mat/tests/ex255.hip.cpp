29057bed1445bd9d248d4f8362418d8d669c97b9
Junchao Zhang <jczhang@anl.gov>
2022-02-04 15:53:42 -0700

    update ex255

--- src/mat/tests/ex255.hip.cpp
+++ src/mat/tests/ex255.hip.cpp
@@ -1,8 +1,12 @@
 #include<hip/hip_runtime.h>
 #include<hip/hip_runtime_api.h>
 #include <mpi.h>
+#include <hipblas.h>
 
 #define CHKERRQ(ierr) assert(ierr == hipSuccess)
+
+#define CHKBLAS(ierr) assert(ierr == HIPBLAS_STATUS_SUCCESS)
+
 
 __global__ void Pack(int N,const double *vec_d,double *buf_d)
 {
@@ -13,19 +17,55 @@ __global__ void Pack(int N,const double *vec_d,double *buf_d)
   }
 }
 
+void CPU_fill(double *v,int m, int n)
+{
+  for(int i=0;i<m*n;i++)
+    v[i] = i;
+}
+
 int main(int argc,char **argv)
 {
   hipError_t  ierr = hipSuccess;
+  hipblasStatus_t stat = HIPBLAS_STATUS_SUCCESS;
   const int   vecLen = 7;
   const int   bufLen = 2*vecLen;
   double      vec_h[vecLen],buf_h[bufLen];
   double      *buf_d,*vec_d;
 
+  hipblasHandle_t handle;
+  double          *A_h,*B_h,*C_h;
+  double          *A_d,*B_d,*C_d;
+  int             m = 64;
+  const double    alf = 1.0f;
+  const double    bet = 0.0f;
+  const double    *alpha = &alf;
+  const double    *beta = &bet;
+
+
   MPI_Init(&argc,&argv);
+
+  stat = hipblasCreate(&handle);CHKBLAS(stat);
+
+  // Allocate 3 arrays on CPU
+  A_h = (double *)malloc(m*m*sizeof(double));
+  B_h = (double *)malloc(m*m*sizeof(double));
+  C_h = (double *)malloc(m*m*sizeof(double));
+  CPU_fill(A_h, m, m);
+  CPU_fill(B_h, m, m);
+  CPU_fill(C_h, m, m);
+
+  // Allocate 3 arrays on GPU
+  ierr = hipMalloc(&A_d, m*m*sizeof(double));CHKERRQ(ierr);
+  ierr = hipMalloc(&B_d, m*m*sizeof(double));CHKERRQ(ierr);
+  ierr = hipMalloc(&C_d, m*m*sizeof(double));CHKERRQ(ierr);
+  ierr = hipMemcpy(A_d,A_h,m*m*sizeof(double),hipMemcpyHostToDevice);CHKERRQ(ierr);
+  ierr = hipMemcpy(B_d,B_h,m*m*sizeof(double),hipMemcpyHostToDevice);CHKERRQ(ierr);
+  ierr = hipMemcpy(C_d,C_h,m*m*sizeof(double),hipMemcpyHostToDevice);CHKERRQ(ierr);
+
   ierr = hipMalloc(&vec_d,vecLen*2*sizeof(double));CHKERRQ(ierr);
   ierr = hipMalloc(&buf_d,bufLen*sizeof(double));CHKERRQ(ierr);
 
-  for (int k=0; k<100; k++) {
+  for (int k=0; k<10; k++) {
     for (int i=0; i<vecLen; i++) {
       memset(vec_h,0,vecLen*sizeof(double));
       vec_h[i] = 1.0;
@@ -34,8 +74,18 @@ int main(int argc,char **argv)
       ierr = hipMemcpyAsync(buf_h,buf_d,bufLen*sizeof(double),hipMemcpyDeviceToHost,NULL);CHKERRQ(ierr);
       ierr = hipStreamSynchronize(NULL);CHKERRQ(ierr);
       assert(buf_h[i] == 1.0 && buf_h[i+vecLen] == 1.0);
+      stat = hipblasDgemm(handle, HIPBLAS_OP_N, HIPBLAS_OP_N, m, m, m, alpha, A_d, m, B_d, m, beta, C_d, m);CHKBLAS(stat);
     }
   }
+
+  free(A_h);
+  free(B_h);
+  free(C_h);
+  ierr = hipFree(A_d);CHKERRQ(ierr);
+  ierr = hipFree(B_d);CHKERRQ(ierr);
+  ierr = hipFree(C_d);CHKERRQ(ierr);
+  stat = hipblasDestroy(handle);CHKBLAS(stat);
+
   ierr = hipFree(vec_d);CHKERRQ(ierr);
   ierr = hipFree(buf_d);CHKERRQ(ierr);
   MPI_Finalize();
