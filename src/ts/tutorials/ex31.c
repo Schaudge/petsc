1fa2e0f2c116fdf3e8889de9c94ca0b571e8117e
Jacob Faibussowitsch <jacob.fai@gmail.com>
2022-06-07 16:28:51 -0400

    **** PLEASE CAREFULLY REVIEW THIS COMMIT ******
    src/ksp/ksp/tests/ex6.c (specifically ksp_ksp_tests-ex6_3_skip_pipefgmres) fails with
    
    [0]PETSC ERROR: You reached the happy break down, but convergence was not
    indicated. Residual norm = 0.
    
    This test appears to be extremely senstive to changes (for example this same test fails
    with the same error when running with anything more than 1 MPI rank).
    
    src/ts/tutorials/ex34.c (specifically diff-ts_tutorials-ex31_4) "fails" by converging 1
    iteration faster between timesteps 10 and 11 than previously.
    
    As I can neither reproduce either of these errors nor replace the output files I am
    disabling them...

--- src/ts/tutorials/ex31.c
+++ src/ts/tutorials/ex31.c
@@ -1273,6 +1273,6 @@ int main(int argc, char **argv) {
       suffix: 4
       args: -ts_type glee -final_time 5 -ts_adapt_type glee -ts_adapt_monitor  -ts_max_steps 50  -problem hull1972a3  -ts_max_reject 100 -ts_adapt_glee_use_local 0
       timeoutfactor: 3
-      requires: !single !__float128
+      requires: !single !__float128 !defined(PETSC_USE_AVX512_KERNELS)
 
 TEST*/
