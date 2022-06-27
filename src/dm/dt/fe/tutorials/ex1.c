c27c893946919330f755c2eff034aaaf18c4cbc4
Toby Isaac <toby.isaac@gmail.com>
2022-06-27 15:34:59 -0400

    WIP: register reference elements with the plotly manager

--- src/dm/dt/fe/tutorials/ex1.c
+++ src/dm/dt/fe/tutorials/ex1.c
@@ -3,7 +3,65 @@ const char help[] = "Visualize PetscFE finite elements with plotly.js";
 #include <petsc.h>
 #include <petscviewersaws.h>
 #include <petsc/private/petscimpl.h>
+#include <petsc/private/dmimpl.h>
 #include <SAWs.h>
+
+static PetscErrorCode PetscObjectSAWsWriteProperty(PetscObject object, const char *property, void *data, int count, SAWs_Memory_type mtype, SAWs_Data_type dtype)
+{
+  const char *name;
+  char buf[BUFSIZ];
+
+  PetscFunctionBegin;
+  PetscCall(PetscObjectGetName(object, &name));
+  PetscCall(PetscSNPrintf(buf, BUFSIZ-1, "/PETSc/Objects/%s/%s", name, property));
+  PetscStackCallSAWs(SAWs_Register,(buf, data, count, mtype, dtype));
+  PetscFunctionReturn(0);
+}
+
+typedef struct _n_PetscFEPlotlyLink *PetscFEPlotlyLink;
+
+struct _n_PetscFEPlotlyLink
+{
+  PetscFE fe;
+  PetscFEPlotlyLink next;
+};
+
+// TODO: if PetscInt != int
+static PetscErrorCode ReferenceElementView_SAWs(DM refel, PetscViewer viewer)
+{
+  PetscFunctionBegin;
+  if (((PetscObject) refel)->amsmem) PetscFunctionReturn(0);
+  PetscCall(PetscObjectName((PetscObject) refel));
+  PetscCall(PetscObjectViewSAWs((PetscObject)refel, viewer));
+  PetscCall(PetscObjectSAWsWriteProperty((PetscObject)refel, "coordinate_dimension", &(refel->dimEmbed), 1, SAWs_READ, SAWs_INT));
+  PetscFunctionReturn(0);
+}
+
+static PetscErrorCode PetscFEPlotlyLinkCreate(PetscFE fe, PetscViewer viewer, PetscFEPlotlyLink *fep_link)
+{
+  PetscDualSpace    dsp;
+  DM                dm;
+  PetscFEPlotlyLink link;
+
+  PetscFunctionBegin;
+  PetscCall(PetscFEGetDualSpace(fe, &dsp));
+  PetscCall(PetscDualSpaceGetDM(dsp, &dm));
+  PetscCall(ReferenceElementView_SAWs(dm, viewer));
+  PetscCall(PetscNew(&link));
+  PetscCall(PetscObjectReference((PetscObject) fe));
+  link->fe = fe;
+  *fep_link = link;
+  PetscFunctionReturn(0);
+}
+
+static PetscErrorCode PetscFEPlotlyLinkDestroy(PetscFEPlotlyLink *fep_link)
+{
+  PetscFunctionBegin;
+  PetscCall(PetscFEDestroy(&(*fep_link)->fe));
+  PetscCall(PetscFree(*fep_link));
+  *fep_link = NULL;
+  PetscFunctionReturn(0);
+}
 
 typedef struct _p_PetscFEPlotly *PetscFEPlotly;
 
@@ -13,53 +71,100 @@ struct _p_PetscFEPlotly
 {
   PETSCHEADER(struct _PetscFEPlotlyOps);
   int number_of_elements;
+  PetscFEPlotlyLink head;
 };
 
 PetscClassId PETSCFEPLOTLY_CLASSID;
 
 static PetscErrorCode PetscFEPlotlyDestroy(PetscFEPlotly *fep)
 {
+  PetscFEPlotlyLink head;
+
   PetscFunctionBegin;
   PetscCall(PetscObjectSAWsBlock((PetscObject) *fep));
+  head = (*fep)->head;
+  while (head) {
+    PetscFEPlotlyLink next = head->next;
+
+    PetscCall(PetscFEPlotlyLinkDestroy(&head));
+    head = next;
+  }
   PetscCall(PetscObjectSAWsViewOff((PetscObject) *fep));
   PetscCall(PetscHeaderDestroy(fep));
   PetscFunctionReturn(0);
 }
 
-static PetscErrorCode PetscFEPlotlyView_SAWS(PetscFEPlotly fep, PetscViewer viewer)
+static PetscErrorCode PetscFEPlotlyView_SAWs(PetscFEPlotly fep, PetscViewer viewer)
 {
-  const char *name;
-  char number_of_elements_str[BUFSIZ];
-
   PetscFunctionBegin;
   PetscCall(PetscObjectViewSAWs((PetscObject) fep, viewer));
-  PetscCall(PetscObjectGetName((PetscObject)fep, &name));
-  PetscCall(PetscSNPrintf(number_of_elements_str, BUFSIZ-1, "/PETSc/Objects/%s/number_of_elements", name));
-  PetscStackCallSAWs(SAWs_Register, (number_of_elements_str, &(fep->number_of_elements), 1, SAWs_READ, SAWs_INT));
-  PetscCall(PetscObjectSAWsTakeAccess((PetscObject)fep));
-  PetscCall(PetscObjectSAWsGrantAccess((PetscObject)fep));
+  PetscCall(PetscObjectName((PetscObject)fep));
+  PetscCall(PetscObjectSAWsWriteProperty((PetscObject)fep, "number_of_elements", &(fep->number_of_elements), 1, SAWs_READ, SAWs_INT ));
   PetscFunctionReturn(0);
 }
 
 static PetscErrorCode PetscFEPlotlyCreate(MPI_Comm comm, PetscFEPlotly *fep)
 {
   PetscFunctionBegin;
-  PetscCall(PetscHeaderCreate(*fep, PETSCFEPLOTLY_CLASSID, "PetscFEPlotly", "PetscFE Plotly Manager", "", comm, PetscFEPlotlyDestroy, PetscFEPlotlyView_SAWS));
+  PetscCall(PetscHeaderCreate(*fep, PETSCFEPLOTLY_CLASSID, "PetscFEPlotly", "PetscFE Plotly Manager", "", comm, PetscFEPlotlyDestroy, PetscFEPlotlyView_SAWs));
   (*fep)->number_of_elements = 0;
   PetscCall(PetscObjectSAWsSetBlock((PetscObject) *fep, PETSC_TRUE));
   PetscFunctionReturn(0);
 }
 
+PetscFEPlotly PetscFEPlotlyManager = NULL;
+
+static PetscErrorCode PetscFEPlotlyAddLink(PetscFEPlotly fep, PetscFEPlotlyLink link)
+{
+  char property[BUFSIZ];
+  int old_number_of_elements;
+  PetscDualSpace dsp;
+  DM refel;
+
+  PetscFunctionBegin;
+  link->next = fep->head;
+  fep->head = link;
+  old_number_of_elements = PetscFEPlotlyManager->number_of_elements;
+  PetscCall(PetscFEGetDualSpace(link->fe, &dsp));
+  PetscCall(PetscDualSpaceGetDM(dsp, &refel));
+  PetscCall(PetscSNPrintf(property, BUFSIZ-1, "%d/reference_element", old_number_of_elements));
+  PetscCall(PetscObjectSAWsWriteProperty((PetscObject) fep, property, (void *) &(((PetscObject) refel)->name), 1, SAWs_READ, SAWs_STRING));
+  PetscFEPlotlyManager->number_of_elements = old_number_of_elements + 1;
+  PetscFunctionReturn(0);
+}
+
+static PetscErrorCode PetscFEView_SAWs_Plotly(PetscFE fe, PetscViewer viewer)
+{
+  PetscFEPlotlyLink link;
+
+  PetscFunctionBegin;
+  PetscCall(PetscFEPlotlyLinkCreate(fe, viewer, &link));
+  if (!PetscFEPlotlyManager) {
+    PetscCall(PetscFEPlotlyCreate(PETSC_COMM_WORLD, &PetscFEPlotlyManager));
+    PetscCall(PetscObjectSetName((PetscObject) PetscFEPlotlyManager, "PetscFEPlotly"));
+    PetscCall(PetscFEPlotlyView_SAWs(PetscFEPlotlyManager, viewer));
+    PetscCall(PetscObjectRegisterDestroy((PetscObject) PetscFEPlotlyManager));
+  }
+  PetscCall(PetscFEPlotlyAddLink(PetscFEPlotlyManager, link));
+  PetscFunctionReturn(0);
+}
+
 int main(int argc, char **argv)
 {
-  PetscFEPlotly fep;
 
   PetscCall(PetscInitialize(&argc, &argv, NULL, help));
   PetscCall(PetscClassIdRegister("PetscFE Plotly Manager",&PETSCFEPLOTLY_CLASSID));
-  PetscCall(PetscFEPlotlyCreate(PETSC_COMM_WORLD, &fep));
-  PetscCall(PetscObjectSetName((PetscObject) fep, "PetscFEPlotly"));
-  PetscCall(PetscFEPlotlyView_SAWS(fep, PETSC_VIEWER_SAWS_(PETSC_COMM_WORLD)));
-  PetscCall(PetscFEPlotlyDestroy(&fep));
+  for (PetscInt dim = 1; dim <= 3; dim++) {
+    PetscFE   fe;
+    PetscInt  Nc = 1;
+    PetscBool isSimplex = PETSC_TRUE;
+    PetscInt  qorder = PETSC_DETERMINE;
+
+    PetscCall(PetscFECreateDefault(PETSC_COMM_WORLD, dim, Nc, isSimplex, NULL, qorder, &fe));
+    PetscCall(PetscFEView_SAWs_Plotly(fe, PETSC_VIEWER_SAWS_(PETSC_COMM_WORLD)));
+    PetscCall(PetscFEDestroy(&fe));
+  }
+  printf("here\n");
   PetscCall(PetscFinalize());
   return 0;
 }
