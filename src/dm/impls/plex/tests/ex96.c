4bf757d81bb993ebfcbc895e0661b606b6bde1aa
Blaise Bourdin <bourdin@mcmaster.ca>
2022-09-01 10:18:57 -0400

    Added MRE demonstrating a bug in DMPlexVecGetClosure in fortran
    Cleaned up example

---
+++ src/dm/impls/plex/tests/ex96.c
@@ -0,0 +1,88 @@
+static char help[] = "Test DMPlexVecGetClosure / DMPlexVecRestoreClosure\n\n";
+
+#include <petsc.h>
+
+int main(int argc,char **argv) {
+  DM              dm,pdm;
+  PetscSection    section;
+  char            ifilename[PETSC_MAX_PATH_LEN];
+  PetscInt        pStart,pEnd;
+  PetscScalar     *cval;
+  PetscInt        clSize;
+  Vec             v;
+
+  PetscFunctionBeginUser;
+  PetscCall(PetscInitialize(&argc,&argv,NULL,help));
+  PetscOptionsBegin(PETSC_COMM_WORLD,NULL,"FEM Layout Options","ex96");
+  PetscCall(PetscOptionsString("-i","Filename to read","ex96",ifilename,ifilename,sizeof(ifilename),NULL));
+  PetscOptionsEnd();
+
+  PetscCall(DMPlexCreateFromFile(PETSC_COMM_WORLD,ifilename,NULL,PETSC_TRUE,&dm));
+  PetscCall(DMPlexDistributeSetDefault(dm,PETSC_FALSE));
+  PetscCall(DMSetFromOptions(dm));
+
+  PetscCall(DMPlexDistribute(dm,0,NULL,&pdm));
+  if (pdm) {
+    PetscCall(DMDestroy(&dm));
+    dm = pdm;
+  }
+  PetscCall(DMViewFromOptions(dm,NULL,"-dm_view"));
+
+  /* create a section */
+  PetscCall(PetscSectionCreate(PetscObjectComm((PetscObject)dm),&section));
+  PetscCall(DMPlexGetChart(dm,&pStart,&pEnd));
+  PetscCall(PetscSectionSetChart(section,pStart,pEnd));
+
+  /* initialize the section storage with a single value at point 0 */
+  PetscCall(PetscSectionSetDof(section,pStart,1));
+
+  PetscCall(PetscSectionSetUp(section));
+  PetscCall(DMSetLocalSection(dm,section));
+  PetscCall(PetscObjectViewFromOptions((PetscObject)section,NULL,"-dm_section_view"));
+
+  PetscCall(DMGetLocalVector(dm,&v));
+  PetscCall(VecSet(v,-1.0));
+  PetscCall(VecViewFromOptions(v,NULL,"-dm_vec_view"));
+
+  // PetscCall(PetscPrintf(PETSC_COMM_WORLD,"Point %" PetscInt_FMT "\n",pStart));
+  // cval = NULL;
+  // PetscCall(DMPlexVecGetClosure(dm,section,v,pStart,&clSize,&cval));
+  // PetscCall(PetscRealView(clSize,cval,PETSC_VIEWER_STDOUT_SELF));
+  // PetscCall(DMPlexVecRestoreClosure(dm,section,v,pStart,&clSize,&cval));
+
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD,"Point %" PetscInt_FMT "\n",pStart+1));
+  cval = NULL;
+  PetscCall(DMPlexVecGetClosure(dm,section,v,pStart+1,&clSize,&cval));
+  PetscCall(PetscRealView(clSize,cval,PETSC_VIEWER_STDOUT_SELF));
+  PetscCall(DMPlexVecRestoreClosure(dm,section,v,pStart+1,&clSize,&cval));
+
+  PetscCall(PetscSectionDestroy(&section));
+  PetscCall(DMRestoreLocalVector(dm,&v));
+  PetscCall(DMDestroy(&dm));
+
+  PetscCall(VecCreate(PETSC_COMM_WORLD,&v));
+  PetscCall(VecSetSizes(v,PETSC_DECIDE,0));
+  PetscCall(PetscObjectSetName((PetscObject) v, "U"));
+  PetscCall(VecSetFromOptions(v));
+  PetscCall(VecViewFromOptions(v,NULL,"-dm_vec_view"));
+  PetscCall(VecGetSize(v,&clSize));
+  PetscCall(PetscPrintf(PETSC_COMM_WORLD,"Vec size: %" PetscInt_FMT "\n",clSize));
+  PetscCall(VecGetArray(v,&cval));
+  PetscCall(PetscRealView(clSize,cval,PETSC_VIEWER_STDOUT_SELF));
+  PetscCall(VecRestoreArray(v,&cval));
+  PetscCall(PetscFinalize()); 
+  return 0;
+}
+
+/*TEST
+  build:
+    requires: exodusii pnetcdf !complex
+  testset:
+    args: -i ${wPETSC_DIR}/share/petsc/datafiles/meshes/SquareFaceSet.exo -dm_view -dm_section_view -dm_vec_view
+    nsize: 1
+
+    test:
+      suffix: 0
+      args:
+
+TEST*/
---
+++ src/dm/impls/plex/tests/ex96f90.F90
@@ -0,0 +1,98 @@
+program ex98f90
+#include "petsc/finclude/petsc.h"
+    use petsc
+    implicit none
+
+    ! Get the fortran kind associated with PetscInt and PetscReal so that we can use literal constants.
+    PetscInt                           :: dummyPetscInt
+    PetscReal                          :: dummyPetscreal
+    integer,parameter                  :: kPI = kind(dummyPetscInt)
+    integer,parameter                  :: kPR = Selected_Real_Kind(Precision(dummyPetscreal))
+
+    type(tDM)                          :: dm,pdm
+    type(tPetscSection)                :: section
+    character(len=PETSC_MAX_PATH_LEN)  :: ifilename,iobuffer
+    PetscInt                           :: pStart,pEnd
+    PetscErrorCode                     :: ierr
+    PetscBool                          :: flg
+    PetscMPIInt                        :: numProc
+    MPI_Comm                           :: comm
+    PetscReal,Dimension(:),Pointer     :: cVal
+    PetscInt                           :: clSize
+    Type(tVec)                         :: v
+
+    PetscCallA(PetscInitialize(ierr))
+    PetscCallMPIA(MPI_Comm_size(PETSC_COMM_WORLD,numProc,ierr))
+
+    PetscCallA(PetscOptionsGetString(PETSC_NULL_OPTIONS,PETSC_NULL_CHARACTER,"-i",ifilename,flg,ierr))
+    if (.not. flg) then
+        SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_ARG_OUTOFRANGE,"missing input file name -i <input file name>")
+    end if
+
+    PetscCallA(DMPlexCreateFromFile(PETSC_COMM_WORLD,ifilename,PETSC_NULL_CHARACTER,PETSC_TRUE,dm,ierr))
+    PetscCallA(DMPlexDistributeSetDefault(dm,PETSC_FALSE,ierr))
+    PetscCallA(DMSetFromOptions(dm,ierr))
+
+    if (numproc > 1) then
+        PetscCallA(DMPlexDistribute(dm,0_kPI,PETSC_NULL_SF,pdm,ierr))
+        PetscCallA(DMDestroy(dm,ierr))
+        dm = pdm
+    end if
+    PetscCallA(DMViewFromOptions(dm,PETSC_NULL_OPTIONS,"-dm_view",ierr))
+
+    PetscCallA(PetscObjectGetComm(dm,comm,ierr))
+    PetscCallA(PetscSectionCreate(comm,section,ierr))
+    PetscCallA(DMPlexGetChart(dm,pStart,pEnd,ierr))
+    PetscCallA(PetscSectionSetChart(section,pStart,pEnd,ierr))
+
+    PetscCallA(PetscSectionSetDof(section,pStart,1_kPI,ierr))
+
+    PetscCallA(PetscSectionSetUp(section,ierr))
+    PetscCallA(DMSetLocalSection(dm,section,ierr))
+    PetscCallA(PetscObjectViewFromOptions(section,PETSC_NULL_OPTIONS,"-dm_section_view",ierr))
+  
+    PetscCallA(DMGetLocalVector(dm,v,ierr))
+    PetscCallA(VecSet(v,-1.0_kPR,ierr))
+    PetscCallA(VecViewFromOptions(v,PETSC_NULL_OPTIONS,"-dm_vec_view",ierr))
+  
+    ! Write(iobuffer,"('Point: ',i0'\n')") pStart
+    ! PetscCallA(PetscPrintf(PETSC_COMM_WORLD,iobuffer,ierr));
+    ! PetscCallA(DMPlexVecGetClosure(dm,section,v,pStart,cval,ierr))
+    ! PetscCallA(PetscRealView(size(cval),cval,PETSC_VIEWER_STDOUT_SELF,ierr))
+    ! PetscCallA(DMPlexVecRestoreClosure(dm,section,v,pStart,cval,ierr))
+  
+    Write(iobuffer,"('Point: ',i0'\n')") pStart+1
+    PetscCallA(PetscPrintf(PETSC_COMM_WORLD,iobuffer,ierr));
+    PetscCallA(DMPlexVecGetClosure(dm,section,v,pStart+1,cval,ierr))
+    PetscCallA(PetscRealView(size(cval),cval,PETSC_VIEWER_STDOUT_SELF,ierr))
+    PetscCallA(DMPlexVecRestoreClosure(dm,section,v,pStart+1,cval,ierr))
+
+    PetscCallA(PetscSectionDestroy(section,ierr))
+    PetscCallA(DMRestoreLocalVector(dm,v,ierr))
+    PetscCallA(DMDestroy(dm,ierr))
+
+    PetscCallA(VecCreate(PETSC_COMM_WORLD,v,ierr))
+    PetscCallA(VecSetSizes(v,PETSC_DECIDE,0,ierr))
+    PetscCallA(PetscObjectSetName(v, "U",ierr))
+    PetscCallA(VecSetFromOptions(v,ierr))
+    PetscCallA(VecViewFromOptions(v,PETSC_NULL_OPTIONS,"-dm_vec_view",ierr))
+    PetscCallA(VecGetSize(v,clSize,ierr))
+    Write(iobuffer,"('Size: ',i0'\n')") clSize
+    PetscCallA(PetscPrintf(PETSC_COMM_WORLD,iobuffer,ierr));
+    PetscCallA(VecGetArrayF90(v,cval,ierr))
+    PetscCallA(PetscRealView(clSize,cval,PETSC_VIEWER_STDOUT_SELF,ierr))
+    PetscCallA(VecRestoreArrayF90(v,cval,ierr))
+
+    PetscCallA(PetscFinalize(ierr))
+end program ex98f90
+
+! /*TEST
+!   build:
+!     requires: exodusii pnetcdf !complex
+!   testset:
+!     args: -i ${wPETSC_DIR}/share/petsc/datafiles/meshes/SquareFaceSet.exo -dm_view -dm_section_view -dm_vec_view
+!     nsize: 1
+!     test:
+!       suffix: 0
+!       args:
+! TEST*/
