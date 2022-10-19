9b0b53e9f9722c17167085bce691e23f1c4b8822
Richard Tran Mills <rmills@rmills.org>
2022-10-19 16:52:36 -0700

    Define REGRESSOR_LINEAR_HEADER for elements common to all linear regression models

--- src/ml/regressor/impls/linear/linearimpl.h
+++ src/ml/regressor/impls/linear/linearimpl.h
@@ -4,22 +4,24 @@
 #include <petsc/private/regressorimpl.h>
 #include <petscksp.h>
 
-typedef struct {
-  /* Note: It might make sense for all of this to eventually be defined as the macro PETSCREGRESSORLINEARHEADER,
-   * since that it may form a "base" that other linear models might use. */
+/* We define this header, since it serves as a "base" for all linear models. */
+#define REGRESSOR_LINEAR_HEADER \
+  /* Parameters of the fitted regression model */           \
+  Vec coefficients;                                         \
+  PetscScalar intercept;                                    \
+                                                            \
+  Mat X;   /* Operator of the linear model; often the training data matrix, but might be a MATCOMPOSITE */ \
+  Mat C;   /* Centering matrix */ \
+  Vec rhs; /* Right-hand side of the linear model; often the target vector, but may be the mean-centered version */ \
+  /* Various options */ \
+  PetscBool fit_intercept  /* Calculate intercept ("bias" or "offset") if true. Assume centered data if false. */
 
-  /* Parameters of the fitted regression model */
-  Vec coefficients;
-  PetscScalar intercept;
+typedef struct {
+  REGRESSOR_LINEAR_HEADER;
 
   KSP ksp;
-  Mat X;   /* Operator passed to the KSP; often the training data matrix, but might be a MATCOMPOSITE */
   Mat XtX; /* Normal matrix formed from X */
-  Mat C;   /* Centering matrix */
-  Vec rhs; /* Right-hand side used with the KSP; often the target vector, but may be the mean-centered version */
 
-  /* Various options */
-  PetscBool fit_intercept;  /* Calculate intercept ("bias" or "offset") if true. Assume centered data if false. */
 } PETSCREGRESSOR_LINEAR;
 
 #endif
