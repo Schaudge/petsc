95d3cf8b31bc96e03f22a0a2849e00e92fd52c76
Hansol Suh <hsuh@compute-386-08.cels.anl.gov>
2023-03-24 19:58:33 -0500

    WIP: gets type:seqdensecuda

--- src/mat/tutorials/ex19.c
+++ src/mat/tutorials/ex19.c
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 const char help[] = "Test VecCreateMatDense()\n\n";
 
+=======
+>>>>>>> 059356d8ed6 (WIP: gets type:seqdensecuda)
 #include <petscdevice_cuda.h>
 #include <petscmat.h>
 #include <petscconf.h>
@@ -7,6 +10,7 @@ const char help[] = "Test VecCreateMatDense()\n\n";
 
 int main(int argc, char **args)
 {
+<<<<<<< HEAD
   Mat      A;
   Vec      X;
   PetscInt N = 20;
@@ -47,6 +51,60 @@ int main(int argc, char **args)
   PetscCall(MatDestroy(&A));
   PetscCall(VecDestroy(&X));
   PetscCall(PetscFinalize());
+=======
+  Mat            A;
+  Vec            X;
+  PetscInt       N = 20, num_threads = 128;
+  PetscLogEvent  event;
+  PetscMPIInt    rank, size;
+  PetscBool      iscuda = PETSC_FALSE, iship = PETSC_FALSE;
+  PetscBool      optionflag, compareflag;
+  char           vectypename[PETSC_MAX_PATH_LEN];
+
+  PetscFunctionBeginUser;
+  PetscCall(PetscInitialize(&argc, &args, (char *)0, NULL));
+  PetscCall(PetscOptionsGetInt(NULL, NULL, "-n", &N, NULL));
+  PetscCall(PetscOptionsGetInt(NULL, NULL, "-num_threads", &num_threads, NULL));
+
+  PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD, &rank));
+  PetscCallMPI(MPI_Comm_size(PETSC_COMM_WORLD, &size));
+
+  PetscCall(PetscOptionsGetString(NULL, NULL, "-vectype", vectypename, sizeof(vectypename), &optionflag));
+  PetscCall(PetscLogEventRegister("GPU operator", MAT_CLASSID, &event));
+
+  if (optionflag) {
+    PetscCall(PetscStrncmp(vectypename, "cuda", (size_t)4, &compareflag));
+    if (compareflag) iscuda = PETSC_TRUE;
+    PetscCall(PetscStrncmp(vectypename, "hip", (size_t)3, &compareflag));
+    if (compareflag) iship = PETSC_TRUE;
+  }
+
+  PetscCall(VecCreate(PETSC_COMM_WORLD, &X));
+  PetscCall(VecSetSizes(X, PETSC_DECIDE, N));
+  if (iscuda) {
+    PetscCall(VecSetType(X, VECCUDA));
+  } else if (iship) {
+    PetscCall(VecSetType(X, VECHIP));
+  } else {
+    PetscCall(VecSetType(X, VECSTANDARD));
+  }
+  PetscCall(VecSetUp(X));
+  PetscCall(PetscObjectSetName((PetscObject)X, "X_commworld"));
+
+  PetscCall(MatCreateDenseFromVecType(X, PETSC_DECIDE, PETSC_DECIDE, N, N, NULL, &A));
+
+  PetscCall(MatSetFromOptions(A));
+  PetscCall(MatSetOption(A, MAT_IGNORE_OFF_PROC_ENTRIES, PETSC_TRUE));
+  PetscCall(MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY));
+  PetscCall(MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY));
+
+  /* test */
+  PetscCall(MatViewFromOptions(A, NULL, "-ex19_mat_view"));
+
+
+  PetscCall(MatDestroy(&A));
+  PetscCall(VecDestroy(&X));
+>>>>>>> 059356d8ed6 (WIP: gets type:seqdensecuda)
   return 0;
 }
 
@@ -55,6 +113,7 @@ int main(int argc, char **args)
    test:
       suffix: cuda
       requires: cuda
+<<<<<<< HEAD
       args: -vec_type cuda -ex19_mat_view
 
    test:
@@ -85,4 +144,8 @@ int main(int argc, char **args)
       suffix: kokkos
       requires: kokkos kokkos_kernels !cuda !hip
       args: -vec_type kokkos -ex19_mat_view
+=======
+      args: -vectype cuda 
+
+>>>>>>> 059356d8ed6 (WIP: gets type:seqdensecuda)
 TEST*/
