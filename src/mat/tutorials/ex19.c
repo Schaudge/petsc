34b91baabe8c13f3203e46dec2e5095068bfe53a
Hansol Suh <hsuh@compute-386-08.cels.anl.gov>
2023-03-30 18:14:35 -0500

    kokkos no gpu test fix

--- src/mat/tutorials/ex19.c
+++ src/mat/tutorials/ex19.c
@@ -83,6 +83,6 @@ int main(int argc, char **args)
 
    test:
       suffix: kokkos
-      requires: kokkos
-      args: -vec_type standard -ex19_mat_view
+      requires: kokkos, !cuda, !hip
+      args: -vec_type kokkos -ex19_mat_view
 TEST*/
