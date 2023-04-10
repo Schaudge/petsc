5db70421e1f4ee7cb92c8610e323e86e4c4cb025
Barry Smith <bsmith@petsc.dev>
2023-04-10 09:25:02 -0500

    netlib-lapack: add back win changes

--- config/BuildSystem/config/packages/netlib-lapack.py
+++ config/BuildSystem/config/packages/netlib-lapack.py
@@ -10,6 +10,7 @@ class Configure(config.package.CMakePackage):
     self.liblist                = [['libnlapack.a','libnblas.a']]
     self.precisions             = ['single','double']
     self.functionsFortran       = 1
+    self.downloadonWindows      = 1
     self.buildLanguages         = ['FC']
     self.minCmakeVersion        = (2,8,3)
     return
@@ -19,7 +20,24 @@ class Configure(config.package.CMakePackage):
     self.compilerFlags = framework.require('config.compilerFlags', self)
     return
 
+  def configureLibrary(self):
+    config.package.Package.configureLibrary(self)
+
   def formCMakeConfigureArgs(self):
     args = config.package.CMakePackage.formCMakeConfigureArgs(self)
     args.append('-DLIBRARY_PREFIX=n')
     return args
+
+  def Install(self):
+    config.package.CMakePackage.Install(self)
+
+    # LAPACK CMake cannot name the generated files with Microsoft compilers with .lib so need to rename them
+    if self.framework.getCompiler().find('win') > -1:
+      import os
+      from shutil import copyfile
+      if os.path.isfile(os.path.join(self.installDir,self.libdir,'libnblas.a')):
+        copyfile(os.path.join(self.installDir,self.libdir,'libnblas.a'),os.path.join(self.installDir,self.libdir,'libnblas.lib'))
+      if os.path.isfile(os.path.join(self.installDir,self.libdir,'libnlapack.a')):
+        copyfile(os.path.join(self.installDir,self.libdir,'libnlapack.a'),os.path.join(self.installDir,self.libdir,'libnlapack.lib'))
+
+    return self.installDir
