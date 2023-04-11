d14fd106f09396c3880ea7b2e45a9a0d4b9af972
Toby Isaac <toby.isaac@gmail.com>
2023-04-11 17:12:34 -0500

    WIP: rewrite of cdbfgs compiles, started implementing CD_REORDER

--- src/ksp/ksp/utils/lmvm/cdbfgs/cdbfgs.h
+++ src/ksp/ksp/utils/lmvm/cdbfgs/cdbfgs.h
@@ -1,5 +1,13 @@
 #include <../src/ksp/ksp/utils/lmvm/lmvm.h>
 
+typedef enum {
+  MATLBFGS_BASIC, // vector by vector
+  MATLBFGS_CD_REORDER, // compact dense, reordering for a single trsm
+  MATLBFGS_CD_INPLACE, // compact dense, simulate trsm in recycle order
+}
+MatLBFGSType;
+PETSC_INTERN const char *const MatLBFGSTypes[];
+
 /*
   Compact dense representation for the limited-memory BFGS method.
 */
@@ -10,14 +18,15 @@ typedef struct {
   PetscInt  idx_begin;                                   // index of the oldest colums in Sfull and Yfull
   Mat       Sfull, Yfull;                                // Stored in recycled order
   Mat       Wfull;                                       // J_0 Y
-  Mat       Rbar;                                        // logically upper-triangular (wrt history ordering) of -S^T Y
+  Mat       StY;
   Mat       C;                                           // diag(S^T Y) + Y^T J_0 Y)
-  PetscInt  watchdog, max_seq_rejects;                   /* tracker to reset after a certain # of consecutive rejects */
   Vec       work_0, work_1, work_2;
+  MatType   dense_type;
+  MatLBFGSType strategy;
+
+  PetscInt  watchdog, max_seq_rejects;                   /* tracker to reset after a certain # of consecutive rejects */
   PetscBool allocated;
   PetscReal delta, delta_min, delta_max;
-  MatType   dense_type;
-  MatCDBFGS_RinvTypes Rinv_type;
 } Mat_CDBFGS;
 
 PETSC_INTERN PetscErrorCode MatView_LMVMCDBFGS(Mat, PetscViewer);
