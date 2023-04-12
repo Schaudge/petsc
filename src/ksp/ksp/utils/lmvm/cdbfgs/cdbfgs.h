a85c1d5f59c2951148dce4e90837aee8c21a94d8
Hansol Suh <hsuh@anl.gov>
2023-04-12 09:01:57 -0500

    wip update

--- src/ksp/ksp/utils/lmvm/cdbfgs/cdbfgs.h
+++ src/ksp/ksp/utils/lmvm/cdbfgs/cdbfgs.h
@@ -1,13 +1,5 @@
 #include <../src/ksp/ksp/utils/lmvm/lmvm.h>
 
-typedef enum {
-  MATLBFGS_BASIC, // vector by vector
-  MATLBFGS_CD_REORDER, // compact dense, reordering for a single trsm
-  MATLBFGS_CD_INPLACE, // compact dense, simulate trsm in recycle order
-}
-MatLBFGSType;
-PETSC_INTERN const char *const MatLBFGSTypes[];
-
 /*
   Compact dense representation for the limited-memory BFGS method.
 */
@@ -16,7 +8,7 @@ typedef struct {
   Mat       diag_bfgs;                                   /* diagonalized Hessian init */
 
   PetscInt  idx_begin;                                   // index of the oldest colums in Sfull and Yfull
-  Mat       Sfull, Yfull;                                // Stored in recycled order
+  Mat       Sfull, Yfull, StYfull;                                // Stored in recycled order
   Mat       Wfull;                                       // J_0 Y
   Mat       StY;
   Mat       C;                                           // diag(S^T Y) + Y^T J_0 Y)
@@ -25,6 +17,7 @@ typedef struct {
   MatLBFGSType strategy;
 
   PetscInt  watchdog, max_seq_rejects;                   /* tracker to reset after a certain # of consecutive rejects */
+  PetscInt *idx_cols, *idx_rows;
   PetscBool allocated;
   PetscReal delta, delta_min, delta_max;
 } Mat_CDBFGS;
