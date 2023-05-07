3e210546fd7ffe8be730aec4e4b5e8f3a0bbc5b3
Satish Balay <balay@mcs.anl.gov>
2023-05-07 13:55:12 -0500

    CI: disable name:petsc-knl-01, linux-cuda-pkgs jobs for current DC outage
    This Commit should not be merged in via any MR. Its set to flag the pipeline as 'fail' via 'flag-failure' job

--- config/examples/arch-ci-linux-pkgs-dbg-ftn-interfaces.py
+++ config/examples/arch-ci-linux-pkgs-dbg-ftn-interfaces.py
@@ -5,6 +5,7 @@ petsc_hash_pkgs=os.path.join(os.getenv('HOME'),'petsc-hash-pkgs')
 
 configure_options = [
   '--package-prefix-hash='+petsc_hash_pkgs,
+  '--with-cudac=0',
   '--with-cc=clang',
   '--with-cxx=clang++',
   '--with-fc=gfortran',
