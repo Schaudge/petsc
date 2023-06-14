0818750376618d21440bec9ce72636281f1ef000
Hansol Suh <hsuh@anl.gov>
2023-06-14 13:28:02 -0500

    better version of user-side cuda obj,grad

--- src/tao/unconstrained/tutorials/rosenbrock1cu.cu
+++ src/tao/unconstrained/tutorials/rosenbrock1cu.cu
@@ -1,18 +1,39 @@
 #include <petscdevice_cuda.h>
 #include <petscdevice.h>
 #include "rosenbrock1.h"
+#include <cuda.h>
 
-__global__ void Rosenbrock1ObjAndGradCUDA_Internal(const PetscScalar x[], PetscScalar g[], PetscReal f[], PetscReal alpha)
+__global__ void Rosenbrock1ObjAndGradCUDA_Internal(const PetscScalar x[], PetscScalar g[], PetscReal f[], PetscReal alpha, PetscInt nn)
 {
   PetscReal t1, t2;
+  int i;
+  int idx = blockIdx.x*blockDim.x+threadIdx.x;//1D grid
+  PetscInt n_threads = 32;
+  PetscInt tid = threadIdx.x;
 
-  t1 = x[1] - x[0]*x[0];
-  t2 = 1 - x[0];
+  __shared__ double f_array[32];
+  f_array[tid] = 0.0;
 
-  g[0] = -4*alpha*(t1)*x[0] - 2*(t2);
-  g[1] = 2*alpha*(t1);
-  atomicAdd_system(f, alpha*t1*t1);//only for compute capability of 6.x +
-  atomicAdd_system(f, t2*t2);//only for compute capability of 6.x +
+  if (idx >= nn) return;
+
+  for (i = 2*tid; i< nn; i+=2*n_threads) {
+    t1 = x[i+1] - x[i]*x[i];
+    t2 = 1 - x[i];
+  
+    g[i] = -4*alpha*(t1)*x[i] - 2.*(t2);
+    g[i+1] = 2*alpha*(t1);
+    f_array[i] = alpha*t1*t1 + t2*t2;
+  }
+
+  // Reduction on f_array
+  for (unsigned int s=blockDim.x/2; s>0; s>>=1) {
+    if (tid < s) {
+      f_array[tid] += f_array[tid+s];
+    }
+    __syncthreads();
+  }
+
+  if (tid ==0) atomicAdd(f,f_array[0]);
 }
 
 PetscErrorCode Rosenbrock1ObjAndGradCUDA(Vec X, Vec G, PetscReal *f, PetscReal alpha, PetscInt nn)
@@ -25,7 +46,8 @@ PetscErrorCode Rosenbrock1ObjAndGradCUDA(Vec X, Vec G, PetscReal *f, PetscReal a
   PetscCall(VecGetArrayAndMemType(G, &g, &memtype_g));
   PetscCall(VecGetArrayReadAndMemType(X, &x, &memtype_x));
 
-  Rosenbrock1ObjAndGradCUDA_Internal<<<1,1>>>(x, g, f, alpha);
+  // n_threads is hardware dependant... Chose 32 for test case. 
+  Rosenbrock1ObjAndGradCUDA_Internal<<<1,32>>>(x, g, f, alpha, nn);
   // reduce all ff values together 
 
   PetscCall(VecRestoreArrayAndMemType(G, &g));
