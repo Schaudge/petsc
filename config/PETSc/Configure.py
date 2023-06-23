fb684468ecd74424434749cc6e74353e5de04ea2
Toby Isaac <toby.isaac@gmail.com>
2023-06-23 11:54:14 -0400

    Configure: add PETSC_DEPRECATED_STRUCT

--- config/PETSc/Configure.py
+++ config/PETSc/Configure.py
@@ -678,6 +678,7 @@ prepend-path PATH "%s"
       checkDeprecated('DEPRECATED_FUNCTION', '{} int myfunc(void) {{ return 1; }}', is_intel)
       checkDeprecated('DEPRECATED_TYPEDEF', 'typedef int my_int {};', is_intel)
       checkDeprecated('DEPRECATED_ENUM', 'enum E {{ oldval {}, newval }};', is_intel)
+      checkDeprecated('DEPRECATED_STRUCT', 'struct S {{ int a; }} {};', is_intel)
       # I was unable to make a CPP macro that takes the old and new values as separate
       # arguments and builds the message needed by _Pragma hence the deprecation message is
       # handled as it is
