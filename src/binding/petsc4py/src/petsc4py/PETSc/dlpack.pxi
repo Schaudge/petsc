Schaudge/petsc

commit b12f50eb112f916bb6f36986fae3f8302242f7a3

petsc4py: Update to Cython 3

src/binding/petsc4py/src/petsc4py/PETSc/dlpack.pxi
@@ -53,15 +53,15 @@ cdef struct DLTensor:
     int64_t* strides
     uint64_t byte_offset
 
-ctypedef int (*dlpack_manager_del_obj)(void*) nogil
+ctypedef int (*dlpack_manager_del_obj)(void*) noexcept nogil
 
 cdef struct DLManagedTensor:
     DLTensor dl_tensor
     void* manager_ctx
-    void (*manager_deleter)(DLManagedTensor*) nogil
+    void (*manager_deleter)(DLManagedTensor*) noexcept nogil
     dlpack_manager_del_obj del_obj
 
-cdef void pycapsule_deleter(object dltensor):
+cdef void pycapsule_deleter(object dltensor) noexcept with gil:
     cdef DLManagedTensor* dlm_tensor = NULL
     try:
         dlm_tensor = <DLManagedTensor *>PyCapsule_GetPointer(dltensor, 'used_dltensor')
@@ -70,7 +70,7 @@ cdef void pycapsule_deleter(object dltensor):
         dlm_tensor = <DLManagedTensor *>PyCapsule_GetPointer(dltensor, 'dltensor')
     manager_deleter(dlm_tensor)
 
-cdef void manager_deleter(DLManagedTensor* tensor) nogil:
+cdef void manager_deleter(DLManagedTensor* tensor) noexcept nogil:
     if tensor.manager_ctx is NULL:
         return
     free(tensor.dl_tensor.shape)

src/binding/petsc4py/conf/confpetsc.py
@@ -68,7 +68,7 @@ except ImportError:
 
 # Cython
 
-CYTHON = '0.29.36'
+CYTHON = '3.0.0'
 
 def cython_req():
     return CYTHON
@@ -100,8 +100,8 @@ def cython_chk(VERSION, verbose=True):
         return False
     REQUIRED = Version(VERSION)
     PROVIDED = Version(m.groups()[0])
-    if PROVIDED != REQUIRED:
-        warn("You need Cython == {0} (you have version {1})"
+    if PROVIDED < REQUIRED:
+        warn("You need Cython >= {0} (you have version {1})"
              .format(VERSION, CYTHON_VERSION))
         return False
     #
@@ -130,7 +130,7 @@ def cython_run(
             return
     finally:
         os.chdir(cwd)
-    require = 'Cython == %s' % VERSION
+    require = 'Cython >= %s' % VERSION
     if setuptools and not cython_chk(VERSION, verbose=False):
         if sys.modules.get('Cython'):
             removed = getattr(sys.modules['Cython'], '__version__', '')

src/binding/petsc4py/src/lib-petsc/compat/cuda.h
@@ -8,13 +8,13 @@
     SETERRQ(PETSC_COMM_SELF,PETSC_ERR_SUP,"%s() requires CUDA",PETSC_FUNCTION_NAME); \
     PetscFunctionReturn(PETSC_ERR_SUP);} while (0)
 
-PetscErrorCode MatDenseCUDAGetArrayRead(Mat A, const PetscScalar **gpuarray) {PetscCUDAError;}
-PetscErrorCode MatDenseCUDARestoreArrayRead(Mat A, const PetscScalar **gpuarray) {PetscCUDAError;}
-PetscErrorCode MatDenseCUDAGetArrayWrite(Mat A, PetscScalar **gpuarray) {PetscCUDAError;}
-PetscErrorCode MatDenseCUDARestoreArrayWrite(Mat A, PetscScalar **gpuarray) {PetscCUDAError;}
-PetscErrorCode MatDenseCUDAGetArray(Mat A, PetscScalar **gpuarray) {PetscCUDAError;}
-PetscErrorCode MatDenseCUDARestoreArray(Mat A, PetscScalar **gpuarray) {PetscCUDAError;}
-PetscErrorCode MatCreateDenseCUDA(MPI_Comm comm, PetscInt m, PetscInt n, PetscInt M, PetscInt N, PetscScalar gpuarray[], Mat *A) {PetscCUDAError;}
+PetscErrorCode MatDenseCUDAGetArrayRead(PETSC_UNUSED Mat A, PETSC_UNUSED const PetscScalar **gpuarray) {PetscCUDAError;}
+PetscErrorCode MatDenseCUDARestoreArrayRead(PETSC_UNUSED Mat A, PETSC_UNUSED const PetscScalar **gpuarray) {PetscCUDAError;}
+PetscErrorCode MatDenseCUDAGetArrayWrite(PETSC_UNUSED Mat A, PETSC_UNUSED PetscScalar **gpuarray) {PetscCUDAError;}
+PetscErrorCode MatDenseCUDARestoreArrayWrite(PETSC_UNUSED Mat A, PETSC_UNUSED PetscScalar **gpuarray) {PetscCUDAError;}
+PetscErrorCode MatDenseCUDAGetArray(PETSC_UNUSED Mat A, PETSC_UNUSED PetscScalar **gpuarray) {PetscCUDAError;}
+PetscErrorCode MatDenseCUDARestoreArray(PETSC_UNUSED Mat A, PETSC_UNUSED PetscScalar **gpuarray) {PetscCUDAError;}
+PetscErrorCode MatCreateDenseCUDA(PETSC_UNUSED MPI_Comm comm, PETSC_UNUSED PetscInt m, PETSC_UNUSED PetscInt n, PETSC_UNUSED PetscInt M, PETSC_UNUSED PetscInt N, PETSC_UNUSED PetscScalar gpuarray[], PETSC_UNUSED Mat *A) {PetscCUDAError;}
 
 #undef PetscCUDAError
 

src/binding/petsc4py/src/lib-petsc/compat/viennacl.h
@@ -8,8 +8,8 @@
     SETERRQ(PETSC_COMM_SELF,PETSC_ERR_SUP,"%s() requires ViennaCL",PETSC_FUNCTION_NAME); \
     PetscFunctionReturn(PETSC_ERR_SUP);} while (0)
 
-PetscErrorCode  VecCreateSeqViennaCLWithArrays(MPI_Comm comm,PetscInt bs,PetscInt n,PetscScalar cpuarray[],PetscScalar* viennaclvec,Vec *V) {PetscViennaCLError;}
-PetscErrorCode VecCreateMPIViennaCLWithArrays(MPI_Comm comm,PetscInt bs,PetscInt n,PetscInt N,PetscScalar cpuarray[],PetscScalar *viennaclvec,Vec *vv) {PetscViennaCLError;}
+PetscErrorCode VecCreateSeqViennaCLWithArrays(PETSC_UNUSED MPI_Comm comm,PETSC_UNUSED PetscInt bs,PETSC_UNUSED PetscInt n,PETSC_UNUSED PetscScalar cpuarray[],PETSC_UNUSED PetscScalar* viennaclvec,PETSC_UNUSED Vec *V) {PetscViennaCLError;}
+PetscErrorCode VecCreateMPIViennaCLWithArrays(PETSC_UNUSED MPI_Comm comm,PETSC_UNUSED PetscInt bs,PETSC_UNUSED PetscInt n,PETSC_UNUSED PetscInt N,PETSC_UNUSED PetscScalar cpuarray[],PETSC_UNUSED PetscScalar *viennaclvec,PETSC_UNUSED Vec *vv) {PetscViennaCLError;}
 
 #undef PetscViennaCLError
 

src/binding/petsc4py/src/petsc4py/PETSc.pxd
@@ -329,4 +329,4 @@ cdef extern from * nogil:
     PETSC_ERROR_INITIAL
     PETSC_ERROR_REPEAT
 
-cdef PetscErrorCode CHKERR(PetscErrorCode) nogil except PETSC_ERR_PYTHON
+cdef PetscErrorCode CHKERR(PetscErrorCode) except PETSC_ERR_PYTHON nogil

src/binding/petsc4py/src/petsc4py/PETSc.pyx
@@ -1,5 +1,6 @@
 #cython: language_level=3str
 #cython: embedsignature=True
+#cython: embedsignature.format=python
 #cython: annotation_typing=False
 #cython: cdivision=True
 #cython: auto_pickle=False
@@ -8,10 +9,9 @@
 #cython: autotestdict=False
 #cython: warn.multiple_declarators=False
 #cython: optimize.use_switch=False
-
 #cython: binding=False
-#from __future__ import annotations
 
-from __future__ import absolute_import
+#from __future__ import annotations
 cimport cython
+
 include "PETSc/PETSc.pyx"

src/binding/petsc4py/src/petsc4py/PETSc/Mat.pyx
@@ -380,37 +380,34 @@ cdef class Mat(Object):
     # binary operations
 
     def __add__(self, other):
-        if isinstance(self, Mat):
-            return mat_add(self, other)
-        else:
-            return mat_radd(other, self)
+        return mat_add(self, other)
+
+    def __radd__(self, other):
+        return mat_radd(self, other)
 
     def __sub__(self, other):
-        if isinstance(self, Mat):
-            return mat_sub(self, other)
-        else:
-            return mat_rsub(other, self)
+        return mat_sub(self, other)
+
+    def __rsub__(self, other):
+        return mat_rsub(self, other)
 
     def __mul__(self, other):
-        if isinstance(self, Mat):
-            if isinstance(other, Vec):
-                return mat_mul_vec(self, other)
-            else:
-                return mat_mul(self, other)
-        else:
-            return mat_rmul(other, self)
+        return mat_mul(self, other)
+
+    def __rmul__(self, other):
+        return mat_rmul(self, other)
 
     def __div__(self, other):
-        if isinstance(self, Mat):
-            return mat_div(self, other)
-        else:
-            return mat_rdiv(other, self)
+        return mat_div(self, other)
+
+    def __rdiv__(self, other):
+        return mat_rdiv(self, other)
 
     def __truediv__(self, other):
-        if isinstance(self, Mat):
-            return mat_div(self, other)
-        else:
-            return mat_rdiv(other, self)
+        return mat_div(self, other)
+
+    def __rtruediv__(self, other):
+        return mat_rdiv(self, other)
 
     #
 

src/binding/petsc4py/src/petsc4py/PETSc/PETSc.pyx
@@ -58,7 +58,7 @@ cdef PetscErrorCode SETERR(PetscErrorCode ierr) with gil:
         PyErr_SetObject(<object>PyExc_RuntimeError, <long>ierr)
     return ierr
 
-cdef inline PetscErrorCode CHKERR(PetscErrorCode ierr) nogil except PETSC_ERR_PYTHON:
+cdef inline PetscErrorCode CHKERR(PetscErrorCode ierr) except PETSC_ERR_PYTHON nogil:
     if ierr == PETSC_SUCCESS:
         return ierr # no error
     if ierr == PETSC_ERR_PYTHON:
@@ -92,7 +92,7 @@ cdef inline int SETERRMPI(int ierr) with gil:
     <void>SETERR(PETSC_ERR_MPI)
     return ierr
 
-cdef inline PetscErrorCode CHKERRMPI(int ierr) nogil except PETSC_ERR_PYTHON:
+cdef inline PetscErrorCode CHKERRMPI(int ierr) except PETSC_ERR_PYTHON nogil:
     if ierr == MPI_SUCCESS:
       return PETSC_SUCCESS
     <void>SETERRMPI(ierr)
@@ -334,7 +334,7 @@ cdef extern from "<stdio.h>" nogil:
     int fprintf(FILE *, char *, ...)
 
 cdef extern from "Python.h":
-    int Py_AtExit(void (*)())
+    int Py_AtExit(void (*)() noexcept nogil)
     void PySys_WriteStderr(char*,...)
 
 cdef extern from * nogil:
@@ -367,7 +367,7 @@ cdef int getinitargs(object args, int *argc, char **argv[]) except -1:
     argc[0] = c; argv[0] = v
     return 0
 
-cdef void delinitargs(int *argc, char **argv[]) nogil:
+cdef void delinitargs(int *argc, char **argv[]) noexcept nogil:
     # dallocate command line arguments
     cdef int i, c = argc[0]
     cdef char** v = argv[0]
@@ -377,7 +377,7 @@ cdef void delinitargs(int *argc, char **argv[]) nogil:
             if  v[i] != NULL: free(v[i])
         free(v)
 
-cdef void finalize() nogil:
+cdef void finalize() noexcept nogil:
     cdef int ierr = 0
     # deallocate command line arguments
     global PyPetsc_Argc, PyPetsc_Argv
@@ -406,15 +406,15 @@ cdef void finalize() nogil:
 
 # --------------------------------------------------------------------
 
-cdef extern from * nogil:
-    PetscErrorCode (*PetscVFPrintf)(FILE*,const char*,va_list)
+cdef extern from *:
+    PetscErrorCode (*PetscVFPrintf)(FILE*,const char*,va_list) except PETSC_ERR_PYTHON nogil
 
-cdef PetscErrorCode (*prevfprintf)(FILE*,const char*,va_list) nogil
+cdef PetscErrorCode (*prevfprintf)(FILE*,const char*,va_list) except PETSC_ERR_PYTHON nogil
 prevfprintf = NULL
 
 cdef PetscErrorCode PetscVFPrintf_PythonStdStream(
     FILE *fd, const char fmt[], va_list ap,
-) with gil:
+) except PETSC_ERR_PYTHON with gil:
     import sys
     cdef char cstring[8192]
     cdef size_t stringlen = sizeof(cstring)
@@ -432,11 +432,11 @@ cdef PetscErrorCode PetscVFPrintf_PythonStdStream(
         ustring = cstring[:final_pos].decode('UTF-8')
         sys.stderr.write(ustring)
     else:
-        PetscVFPrintfDefault(fd, fmt, ap)
+        CHKERR( PetscVFPrintfDefault(fd, fmt, ap) )
     return PETSC_SUCCESS
 
 cdef int _push_vfprintf(
-    PetscErrorCode (*vfprintf)(FILE*, const char*, va_list) nogil,
+    PetscErrorCode (*vfprintf)(FILE*, const char*, va_list) except PETSC_ERR_PYTHON nogil,
 ) except -1:
     global PetscVFPrintf, prevfprintf
     assert prevfprintf == NULL

commit d0241be5e5d36d13fd9ff2553c73a8dba76cdf0a

Fix seg fault caused by Cython update
The seg fault was introduced by b12f50eb112f916bb6f36986fae3f8302242f7a3.

src/binding/petsc4py/src/petsc4py/PETSc/dlpack.pxi
@@ -61,14 +61,12 @@ cdef struct DLManagedTensor:
     void (*manager_deleter)(DLManagedTensor*) noexcept nogil
     dlpack_manager_del_obj del_obj
 
-cdef void pycapsule_deleter(object dltensor) noexcept with gil:
+cdef void pycapsule_deleter(object dltensor) noexcept:
     cdef DLManagedTensor* dlm_tensor = NULL
-    try:
-        dlm_tensor = <DLManagedTensor *>PyCapsule_GetPointer(dltensor, 'used_dltensor')
-        return # we do not call a used capsule's deleter
-    except Exception:
-        dlm_tensor = <DLManagedTensor *>PyCapsule_GetPointer(dltensor, 'dltensor')
-    manager_deleter(dlm_tensor)
+    # we do not call a used capsule's deleter
+    if PyCapsule_IsValid(dltensor, b'dltensor'):
+        dlm_tensor = <DLManagedTensor *>PyCapsule_GetPointer(dltensor, b'dltensor')
+        manager_deleter(dlm_tensor)
 
 cdef void manager_deleter(DLManagedTensor* tensor) noexcept nogil:
     if tensor.manager_ctx is NULL: