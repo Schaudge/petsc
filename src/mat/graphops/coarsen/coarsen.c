3835b47098ecebef7339936ed327facdb10effdb
Mark Adams <mfadams@lbl.gov>
2023-11-08 13:42:06 -0500

    remov DM access in Mat

--- src/mat/graphops/coarsen/coarsen.c
+++ src/mat/graphops/coarsen/coarsen.c
@@ -1,5 +1,4 @@
 #include <petsc/private/matimpl.h> /*I "petscmatcoarsen.h" I*/
-#include <petscdm.h>
 
 /* Logging support */
 PetscClassId MAT_COARSEN_CLASSID;
@@ -96,76 +95,6 @@ PetscErrorCode MatCoarsenApply(MatCoarsen coarser)
   PetscCall(PetscLogEventBegin(MAT_Coarsen, coarser, 0, 0, 0));
   PetscUseTypeMethod(coarser, apply);
   PetscCall(PetscLogEventEnd(MAT_Coarsen, coarser, 0, 0, 0));
-#if 0
-  Mat mat = coarser->graph;
-  DM  dm;
-  PetscCall(MatGetDM(mat, &dm)); // for aggregate vize
-  if (dm) {
-    PetscInt        nis, color;
-    IS             *asm_iss;
-    Vec             u;
-    static PetscInt counter = 0;
-    PetscCall(DMCreateGlobalVector(dm, &u));
-    PetscCall(PetscCDGetASMBlocks(coarser->agg_lists, 1, &nis, &asm_iss));
-    char argstr1[256] = "-asm_0";
-    char argstr2[256] = "-asm_0";
-    argstr1[5] += counter;
-    argstr2[5] += counter++;
-    PetscCall(PetscStrcat(argstr1, "_dm_view"));
-    PetscCall(PetscStrcat(argstr2, "_vec_view"));
-    if (1) {
-      PetscReal   vv;
-      PetscRandom rnd;
-      PetscCall(PetscRandomCreate(PETSC_COMM_SELF, &rnd));
-      PetscCall(PetscRandomSetInterval(rnd, 0, 10.));
-      PetscCall(PetscRandomSetFromOptions(rnd));
-      for (PetscInt bidx = 0, nn; bidx < nis; bidx++) {
-        IS              is = asm_iss[bidx];
-        const PetscInt *points;
-        PetscCall(ISGetLocalSize(is, &nn));
-        PetscCall(ISGetIndices(is, &points));
-        PetscCall(PetscRandomGetValueReal(rnd, &vv));
-        color = (PetscInt)vv;
-        for (int jj = 0; jj < nn; jj++) {
-          int         gidx    = points[jj];
-          PetscScalar blk_idx = (PetscScalar)(color);
-          PetscCall(VecSetValue(u, gidx, blk_idx, INSERT_VALUES));
-        }
-        PetscCall(ISRestoreIndices(is, &points));
-        PetscCall(ISDestroy(&is));
-      }
-      PetscCall(PetscFree(asm_iss));
-      PetscCall(PetscRandomDestroy(&rnd));
-    } else {
-      PetscInt mstarts, inis, irnis, nn, my0_blk;
-      PetscCall(MatGetOwnershipRange(mat, &mstarts, NULL));
-      /* PetscCall(PCASMGetLocalSubdomains(subpc, &nis, &asm_iss, NULL)); */
-      inis = nis;
-      PetscCallMPI(MPI_Scan(&inis, &irnis, 1, MPI_INT, MPI_SUM, PETSC_COMM_WORLD));
-      my0_blk = irnis - inis;
-      for (PetscInt bidx = 0; bidx < nis; bidx++) {
-        IS              is = asm_iss[bidx];
-        const PetscInt *points;
-        PetscCall(ISGetLocalSize(is, &nn));
-        PetscCall(ISGetIndices(is, &points));
-        for (int jj = 0; jj < nn; jj++) {
-          int         gidx    = points[jj];
-          PetscScalar blk_idx = (PetscScalar)((my0_blk + bidx + 1) % 10);
-          PetscCall(VecSetValue(u, gidx, blk_idx, INSERT_VALUES));
-        }
-        PetscCall(ISRestoreIndices(is, &points));
-      }
-    }
-    PetscCall(VecAssemblyBegin(u));
-    PetscCall(VecAssemblyEnd(u));
-    /* PetscCall(VecView(u, PETSC_VIEWER_STDOUT_WORLD)); */
-    PetscCall(DMSetOutputSequenceNumber(dm, 0, 0));
-    PetscCall(PetscObjectSetName((PetscObject)u, "aggs"));
-    PetscCall(DMViewFromOptions(dm, NULL, argstr1));
-    PetscCall(VecViewFromOptions(u, NULL, argstr2));
-    PetscCall(VecDestroy(&u));
-  }
-#endif
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
