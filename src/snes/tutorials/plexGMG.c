7a950b2ac3c1cb6335fa3f90556be211be7357ae
Matthew G. Knepley <knepley@gmail.com>
2023-11-11 11:02:02 -0500

    SNES plexGMG: Step 1   We add a struct for user options, which are processed by PETSc, and shown using -help.

--- src/snes/tutorials/plexGMG.c
+++ src/snes/tutorials/plexGMG.c
@@ -7,14 +7,36 @@ This example supports automatic convergence estimation.\n\n\n";
   This example can be run using the test system:
 
     make -f ./gmakefile test search="snes_tutorials-plexGMG_*"
+
+  You can see the help using
+
+    make -f ./gmakefile test search="snes_tutorials-plexGMG_*" EXTRA_OPTIONS="-help"
 */
 
 #include <petscdmplex.h>
 
-int main(int argc, char **argv)
+typedef struct {
+  PetscBool viewError; // Output the solution error
+} AppCtx;
+
+static PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options)
 {
   PetscFunctionBeginUser;
+  options->viewError = PETSC_FALSE;
+
+  PetscOptionsBegin(comm, "", "Poisson Problem Options", "DMPLEX");
+  PetscCall(PetscOptionsBool("-error_view", "Output the solution error", "plexGMG.c", options->viewError, &options->viewError, NULL));
+  PetscOptionsEnd();
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+  AppCtx user; // User-defined work context
+
+  PetscFunctionBeginUser;
   PetscCall(PetscInitialize(&argc, &argv, NULL, help));
+  PetscCall(ProcessOptions(PETSC_COMM_WORLD, &user));
   PetscCall(PetscFinalize());
   return 0;
 }
