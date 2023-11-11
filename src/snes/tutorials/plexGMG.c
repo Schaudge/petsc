4339c1cfa40a88ad2ec4759fd72a5cbeee3d9ab1
Matthew G. Knepley <knepley@gmail.com>
2023-11-11 11:07:40 -0500

    SNES plexGMG: Step 2   We add code for creating a DMPlex mesh. We can control the mesh creation and refinement using only options.

--- src/snes/tutorials/plexGMG.c
+++ src/snes/tutorials/plexGMG.c
@@ -11,6 +11,14 @@ This example supports automatic convergence estimation.\n\n\n";
   You can see the help using
 
     make -f ./gmakefile test search="snes_tutorials-plexGMG_*" EXTRA_OPTIONS="-help"
+
+  The mesh can be viewed using
+
+    make -f ./gmakefile test search="snes_tutorials-plexGMG_*" EXTRA_OPTIONS="-dm_view"
+
+  and many formats are supported such as VTK, HDF5, and drawing,
+
+    make -f ./gmakefile test search="snes_tutorials-plexGMG_*" EXTRA_OPTIONS="-dm_view draw -draw_pause -1"
 */
 
 #include <petscdmplex.h>
@@ -30,13 +38,27 @@ static PetscErrorCode ProcessOptions(MPI_Comm comm, AppCtx *options)
   PetscFunctionReturn(PETSC_SUCCESS);
 }
 
+static PetscErrorCode CreateMesh(MPI_Comm comm, AppCtx *user, DM *dm)
+{
+  PetscFunctionBeginUser;
+  PetscCall(DMCreate(comm, dm));
+  PetscCall(DMSetType(*dm, DMPLEX));
+  PetscCall(DMSetFromOptions(*dm));
+  PetscCall(DMSetApplicationContext(*dm, user));
+  PetscCall(DMViewFromOptions(*dm, NULL, "-dm_view"));
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
 int main(int argc, char **argv)
 {
+  DM     dm;   // DMPLEX mesh
   AppCtx user; // User-defined work context
 
   PetscFunctionBeginUser;
   PetscCall(PetscInitialize(&argc, &argv, NULL, help));
   PetscCall(ProcessOptions(PETSC_COMM_WORLD, &user));
+  PetscCall(CreateMesh(PETSC_COMM_WORLD, &user, &dm));
+  PetscCall(DMDestroy(&dm));
   PetscCall(PetscFinalize());
   return 0;
 }
@@ -45,7 +67,7 @@ int main(int argc, char **argv)
 
   test:
     suffix: 2d_p1_gmg_vcycle
-    requires:
+    requires: triangle
     args:
 
 TEST*/
