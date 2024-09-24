ed73dcd05a335e9cd615f6479ffa27efcbb7db55
Toby Isaac <toby.isaac@gmail.com>
2024-09-24 09:18:43 -0400

    VecNest: Add VecNestGetSubVecsRead()/VecNestRestoreSubVecsRead()

--- include/petscvec.h
+++ include/petscvec.h
@@ -785,6 +785,8 @@ PETSC_EXTERN PetscErrorCode VecCreateMPIKokkosWithArray(MPI_Comm, PetscInt, Pets
 #endif
 
 PETSC_EXTERN PetscErrorCode VecNestGetSubVecs(Vec, PetscInt *, Vec **);
+PETSC_EXTERN PetscErrorCode VecNestGetSubVecsRead(Vec, PetscInt *, Vec **);
+PETSC_EXTERN PetscErrorCode VecNestRestoreSubVecsRead(Vec, PetscInt *, Vec **);
 PETSC_EXTERN PetscErrorCode VecNestGetSubVec(Vec, PetscInt, Vec *);
 PETSC_EXTERN PetscErrorCode VecNestSetSubVecs(Vec, PetscInt, PetscInt *, Vec *);
 PETSC_EXTERN PetscErrorCode VecNestSetSubVec(Vec, PetscInt, Vec);
