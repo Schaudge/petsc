63330106982e24475c3bfef31b971250a671f28b
Toby Isaac <toby.isaac@gmail.com>
2024-09-26 16:47:46 -0400

    MATH2OPUS: resolve name conflict of internal function

--- src/mat/impls/h2opus/cuda/math2opus.cu
+++ src/mat/impls/h2opus/cuda/math2opus.cu
@@ -17,7 +17,7 @@
 
 /* math2opusutils */
 PETSC_INTERN PetscErrorCode MatDenseGetH2OpusStridedSF(Mat, PetscSF, PetscSF *);
-PETSC_INTERN PetscErrorCode VecSign(Vec, Vec);
+PETSC_INTERN PetscErrorCode VecSignOutOfPlace(Vec, Vec);
 PETSC_INTERN PetscErrorCode VecSetDelta(Vec, PetscInt);
 PETSC_INTERN PetscErrorCode MatApproximateNorm_Private(Mat, NormType, PetscInt, PetscReal *);
 
