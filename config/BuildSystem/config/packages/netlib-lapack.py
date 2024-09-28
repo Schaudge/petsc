7c60420bd7dad44215e8d81cd7bc03a25d559717
Satish Balay <balay@mcs.anl.gov>
2024-09-28 02:38:57 -0500

    revert

--- config/BuildSystem/config/packages/netlib-lapack.py
+++ config/BuildSystem/config/packages/netlib-lapack.py
@@ -7,7 +7,7 @@ class Configure(config.package.CMakePackage):
     self.download               = ['git://https://github.com/Reference-LAPACK/lapack.git','https://github.com/Reference-LAPACK/lapack/archive/'+self.gitcommit+'.tar.gz']
     self.downloaddirnames       = ['netlib-lapack','lapack']
     self.includes               = []
-    self.liblist                = [['liblapacke.a','libcblas.a','liblapack.a','libblas.a'], ['libnlapack.a','libnblas.a']]
+    self.liblist                = [['libnlapack.a','libnblas.a']]
     self.precisions             = ['single','double']
     self.functionsFortran       = 1
     self.buildLanguages         = ['FC']
