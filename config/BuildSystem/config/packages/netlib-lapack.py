dec4a7e585e39c4366ccf2248129a9087b1ea228
Jose E. Roman <jroman@dsic.upv.es>
2024-10-09 16:40:04 +0000

    configure: add new option in NETLIB-LAPACK to build the C interface (required by PASTIX)

--- config/BuildSystem/config/packages/netlib-lapack.py
+++ config/BuildSystem/config/packages/netlib-lapack.py
@@ -14,6 +14,12 @@ class Configure(config.package.CMakePackage):
     self.minCmakeVersion        = (2,8,3)
     return
 
+  def setupHelp(self, help):
+    import nargs
+    config.package.Package.setupHelp(self, help)
+    help.addArgument(self.PACKAGE,'-download-netlib-lapack-cinterface=<bool>',nargs.ArgBool(None,0,'Build the C interface (CBLAS and LAPACKE) for '+self.name ))
+    return
+
   def setupDependencies(self, framework):
     config.package.CMakePackage.setupDependencies(self, framework)
     self.compilerFlags = framework.require('config.compilerFlags', self)
@@ -21,10 +27,21 @@ class Configure(config.package.CMakePackage):
 
   def formCMakeConfigureArgs(self):
     args = config.package.CMakePackage.formCMakeConfigureArgs(self)
-    if self.argDB['download-pastix']:
+    if not self.cinterface:
+      # use a prefix to avoid conflict with another LAPACK already installed in the system
+      args.append('-DLIBRARY_PREFIX=n')
+    else:
+      # build the C interface; in this case we do not use a prefix because PaStiX build
+      # system does not support it (requires CBLAS/LAPACKE)
       args.append('-DCBLAS:BOOL=ON')
       args.append('-DLAPACKE:BOOL=ON')
-      self.liblist = [['liblapacke.a','libcblas.a','liblapack.a','libblas.a']]
-    else:
-      args.append('-DLIBRARY_PREFIX=n')
     return args
+
+  def generateLibList(self, framework):
+    if self.cinterface:
+      self.liblist = [['liblapacke.a','libcblas.a','liblapack.a','libblas.a']]
+    return config.package.Package.generateLibList(self, framework)
+
+  def configureLibrary(self):
+    self.cinterface = self.argDB['download-netlib-lapack-cinterface']
+    config.package.Package.configureLibrary(self)
